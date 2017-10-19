66cf5b0d271f5f80b22e03b8d49726ea44fc12c8
Aaron Lun <alun@wehi.edu.au>
2017-10-19 08:26:23 +0100

    Fixed URNG to use fixed max(), improved safety of return.

--- src/utils.h
+++ src/utils.h
@@ -5,10 +5,11 @@
 
 struct R_RNG {
     typedef size_t result_type;
-    static constexpr result_type largest=result_type(-1); // Getting the maximum value.
     static constexpr result_type min() { return 0; }
-    static constexpr result_type max() { return largest-1; } 
-    result_type operator()() const { return unif_rand()*largest; }
+    static constexpr result_type max() { return 999999; } // size_t probably holds 1 million okay. Setting this to size_t(-1)-1 doesn't work well.
+    result_type operator()() const { 
+        return std::min(R_RNG::max(), result_type(unif_rand()*double(R_RNG::max()+1))); // Enforce [min(), max()] output in case of failed truncation due to precision.
+    }
 };
 
 // Check subset vector.
