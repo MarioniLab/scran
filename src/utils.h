f35ce3f3ec15210a64249397a1db98c126950b52
Aaron Lun <alun@wehi.edu.au>
2017-10-19 08:53:08 +0100

    Commented on URNG code choices.

--- src/utils.h
+++ src/utils.h
@@ -5,10 +5,18 @@
 
 struct R_RNG {
     typedef size_t result_type;
+
     static constexpr result_type min() { return 0; }
-    static constexpr result_type max() { return 999999; } // size_t probably holds 1 million okay. Setting this to size_t(-1)-1 doesn't work well.
+
+    /* size_t holds 1 million on any platform where R runs, so this shouldn't be an issue. 
+     * Setting this to size_t(-1)-1 seems more portable, but it doesn't work for some reason.
+     * Possibly becaues it can't be held precisely upon coercion into a double()? */
+    static constexpr result_type max() { return 999999; } 
+
     result_type operator()() const { 
-        return std::min(R_RNG::max(), result_type(unif_rand()*double(R_RNG::max()+1))); // Enforce [min(), max()] output in case of failed truncation due to precision.
+        /* Enforce [min(), max()] output in case of failed truncation.
+         * Theoretically possible due to numerical imprecision with doubles. */
+        return std::min(R_RNG::max(), result_type(unif_rand()*double(R_RNG::max()+1))); 
     }
 };
 
