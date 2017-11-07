5d4cb4fbbf32e05e2754b5cae1dd5b10053ec25d
Aaron Lun <alun@wehi.edu.au>
2017-11-07 16:54:23 +0000

    Added explicit argument to turn off min.mean warning. Bumped version number, date.

--- R/computeSumFactors.R
+++ R/computeSumFactors.R
@@ -1,5 +1,5 @@
 .computeSumFactors <- function(x, sizes=seq(20, 100, 5), clusters=NULL, ref.clust=NULL, 
-                               positive=FALSE, errors=FALSE, min.mean=1, subset.row=NULL) 
+                               positive=FALSE, errors=FALSE, min.mean=1, subset.row=NULL, no.warn=FALSE) 
 # This contains the function that performs normalization on the summed counts.
 # It also provides support for normalization within clusters, and then between
 # clusters to make things comparable. It can also switch to linear inverse models
@@ -27,7 +27,7 @@
     }
 
     # Checking the subsetting (with interplay with min.mean if required).
-    if (!is.null(subset.row) && !is.null(min.mean) && is.null(attr(min.mean, "noWarn"))) {
+    if (!is.null(subset.row) && !is.null(min.mean) && !no.warn) {
         warning("'min.mean' should be NULL if filtering on abundance in 'subset.row'")
     }
     subset.row <- .subset_to_index(subset.row, x, byrow=TRUE)
@@ -169,14 +169,17 @@ setGeneric("computeSumFactors", function(x, ...) standardGeneric("computeSumFact
 setMethod("computeSumFactors", "ANY", .computeSumFactors)
 
 setMethod("computeSumFactors", "SingleCellExperiment", 
-          function(x, min.mean=1, subset.row=NULL, ..., assay.type="counts", get.spikes=FALSE, sf.out=FALSE) { 
+          function(x, min.mean=1, subset.row=NULL, no.warn=FALSE, ..., 
+                   assay.type="counts", get.spikes=FALSE, sf.out=FALSE) { 
  
     if (is.null(subset.row) && !is.null(min.mean)) {
-        attr(min.mean, "noWarn") <- 1 # avoid triggering error when subset.row is not user-specified.
+        no.warn <- TRUE # avoid triggering the message of the subsetting was not originally specified
     }
 
     subset.row <- .SCE_subset_genes(subset.row=subset.row, x=x, get.spikes=get.spikes)
-    sf <- .computeSumFactors(assay(x, i=assay.type), subset.row=subset.row, min.mean=min.mean, ...) 
+    sf <- .computeSumFactors(assay(x, i=assay.type), subset.row=subset.row, 
+                             min.mean=min.mean, no.warn=no.warn, ...) 
+
     if (sf.out) { 
         return(sf) 
     }
