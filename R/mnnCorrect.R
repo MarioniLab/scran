a9d740a52874ff68f45e3cd5fe706015d42d7376
Aaron Lun <alun@wehi.edu.au>
2017-11-10 18:38:49 +0000

    Split off data mangling into a separate function. Returned angle for each correction vector if requested.

--- R/mnnCorrect.R
+++ R/mnnCorrect.R
@@ -15,7 +15,135 @@ mnnCorrect <- function(..., k=20, sigma=1, cos.norm.in=TRUE, cos.norm.out=TRUE,
     if (nbatches < 2L) { 
         stop("at least two batches must be specified") 
     }
+    
+    prep.out <- prepare.input.data(batches, cos.norm.in=cos.norm.in, cos.norm.out=cos.norm.out, subset.row=subset.row)
+    in.batches <- prep.out$In
+    out.batches <- prep.out$Out
+    subset.row <- prep.out$Subset
+    same.set <- prep.out$Same
+
+    # Setting up the order.
+    if (is.null(order)) {
+        order <- seq_len(nbatches)
+    } else {
+        order <- as.integer(order)
+        if (!identical(seq_len(nbatches), sort(order))) { 
+            stop(sprintf("'order' should contain values in 1:%i", nbatches))
+        }
+    }
    
+    # Setting up the variables.
+    ref <- order[1]
+    ref.batch.in <- t(in.batches[[ref]])
+    if (!same.set) { 
+        ref.batch.out <- t(out.batches[[ref]])
+    }
+    output <- vector("list", nbatches)
+    output[[ref]] <- out.batches[[ref]]
+    mnn.list <- vector("list", nbatches)
+    mnn.list[[ref]] <- DataFrame(current.cell=integer(0), other.cell=integer(0), other.batch=integer(0))
+    original.batch <- rep(ref, nrow(ref.batch.in)) 
+    angle.list <- vector("list", nbatches)
+    angle.list[[ref]] <- numeric(0)
+
+    # Looping through the batches.
+    for (b in 2:nbatches) { 
+        target <- order[b]
+        other.batch.in.untrans <- in.batches[[target]]
+        other.batch.in <- t(other.batch.in.untrans)
+        if (!same.set) { 
+            other.batch.out.untrans <- out.batches[[target]]
+            other.batch.out <- t(other.batch.out.untrans)
+        }
+        
+        # Finding pairs of mutual nearest neighbours.
+        sets <- find.mutual.nn(ref.batch.in, other.batch.in, k1=k, k2=k, BPPARAM=BPPARAM)
+        s1 <- sets$first
+        s2 <- sets$second      
+
+        # Computing the correction vector.
+        correction.in <- compute.correction.vectors(ref.batch.in, other.batch.in, s1, s2, other.batch.in.untrans, sigma)
+        if (!same.set) {
+            correction.out <- compute.correction.vectors(ref.batch.out, other.batch.out, s1, s2, other.batch.in.untrans, sigma)
+            # NOTE: use of 'other.batch.in.untrans' here is intentional, 
+            # as the distances should be the same as the MNN distances.
+        }
+
+        # Calculating the smallest angle between each correction vector and the first 2 basis vectors of the reference batch.
+        if (compute.angle) {
+            ref.centred <- t(ref.batch.in)
+            ref.centred <- ref.centred - rowMeans(ref.centred) 
+            ref.basis <- .svd_internal(ref.centred, nu=2, nv=0, pc.approx=pc.approx, irlba.args=irlba.args)$u
+
+            angle.out <- numeric(nrow(correction.in))
+            for (i in seq_along(angle.out)) {
+                angle.out[i] <- find.shared.subspace(ref.basis, t(correction.in[i,,drop=FALSE]))$angle
+            }
+            angle.list[[target]] <- angle.out
+        }
+
+        # Removing any component of the correction vector that's parallel to the biological basis vectors in either batch.
+        # Only using cells involved in MNN pairs, to avoid undercorrection in directions that weren't problematic anyway
+        # (given that using SVDs was intended to mitigate the effect of identifying the wrong MNN pairs).
+        if (svd.dim>0) {
+            u1 <- unique(s1)
+            u2 <- unique(s2)
+
+            # Computing the biological subspace in both batches, and subtract it from the batch correction vector.
+            in.span1 <- get.bio.span(t(ref.batch.in[u1,,drop=FALSE]), ndim=svd.dim,
+                                     pc.approx=pc.approx, irlba.args=irlba.args)
+            in.span2 <- get.bio.span(other.batch.in.untrans[,u2,drop=FALSE], ndim=svd.dim, 
+                                     pc.approx=pc.approx, irlba.args=irlba.args)
+            correction.in <- subtract.bio(correction.in, in.span1, in.span2)
+
+            # Repeating for the output values.
+            if (!same.set) { 
+                out.span1 <- get.bio.span(t(ref.batch.out[u1,,drop=FALSE]), subset.row=subset.row,
+                                          ndim=svd.dim, pc.approx=pc.approx, irlba.args=irlba.args)
+                out.span2 <- get.bio.span(other.batch.out.untrans[,u2,drop=FALSE], subset.row=subset.row,
+                                          ndim=svd.dim, pc.approx=pc.approx, irlba.args=irlba.args)
+                correction.out <- subtract.bio(correction.out, out.span1, out.span2, subset.row=subset.row)
+            }
+        } 
+       
+        # Adjusting the shift variance; done after any SVD so that variance along the correction vector is purely technical.
+        if (var.adj) { 
+            correction.in <- adjust.shift.variance(ref.batch.in, other.batch.in, correction.in)
+            if (!same.set) {
+                correction.out <- adjust.shift.variance(ref.batch.out, other.batch.out, correction.out, subset.row=subset.row) 
+            }
+        }
+
+        # Applying the correction and expanding the reference batch. 
+        other.batch.in <- other.batch.in + correction.in
+        ref.batch.in <- rbind(ref.batch.in, other.batch.in)
+        if (same.set) {
+            output[[target]] <- t(other.batch.in)
+        } else {
+            other.batch.out <- other.batch.out + correction.out
+            ref.batch.out <- rbind(ref.batch.out, other.batch.out)
+            output[[target]] <- t(other.batch.out)
+        }
+
+        # Storing the identities of the MNN pairs (using RLEs for compression of runs).
+        mnn.list[[target]] <- DataFrame(current.cell=s2, other.cell=Rle(s1), other.batch=Rle(original.batch[s1]))
+        original.batch <- c(original.batch, rep(target, nrow(other.batch.in)))
+    }
+
+    # Formatting output to be consistent with input.
+    names(output) <- names(batches)
+    names(mnn.list) <- names(batches)
+    final <- list(corrected=output, pairs=mnn.list)
+    if (compute.angle){ 
+        names(angle.list) <- names(batches)
+        final$angles <- angle.list
+    }
+    return(final)
+}
+
+prepare.input.data <- function(batches, cos.norm.in, cos.norm.out, subset.row) {
+    nbatches <- length(batches)
+
     # Checking for identical number of rows (and rownames).
     first <- batches[[1]]
     ref.nrow <- nrow(first)
@@ -66,123 +194,7 @@ mnnCorrect <- function(..., k=20, sigma=1, cos.norm.in=TRUE, cos.norm.out=TRUE,
         same.set <- FALSE
     }
 
-    # Setting up the order.
-    if (is.null(order)) {
-        order <- seq_len(nbatches)
-    } else {
-        order <- as.integer(order)
-        if (!identical(seq_len(nbatches), sort(order))) { 
-            stop(sprintf("'order' should contain values in 1:%i", nbatches))
-        }
-    }
-   
-    # Setting up the variables.
-    ref <- order[1]
-    ref.batch.in <- t(in.batches[[ref]])
-    if (!same.set) { 
-        ref.batch.out <- t(out.batches[[ref]])
-    }
-
-    output <- vector("list", nbatches)
-    output[[ref]] <- out.batches[[ref]]
-
-    mnn.list <- vector("list", nbatches)
-    mnn.list[[ref]] <- DataFrame(current.cell=integer(0), other.cell=integer(0), other.batch=integer(0))
-    original.batch <- rep(ref, nrow(ref.batch.in)) 
-
-    angle.list <- vector("list", nbatches)
-    angle.list[[ref]] <- numeric(0)
-
-    # Looping through the batches.
-    for (b in 2:nbatches) { 
-        target <- order[b]
-        other.batch.in.untrans <- in.batches[[target]]
-        other.batch.in <- t(other.batch.in.untrans)
-        if (!same.set) { 
-            other.batch.out.untrans <- out.batches[[target]]
-            other.batch.out <- t(other.batch.out.untrans)
-        }
-        
-        # Finding pairs of mutual nearest neighbours.
-        sets <- find.mutual.nn(ref.batch.in, other.batch.in, k1=k, k2=k, BPPARAM=BPPARAM)
-        s1 <- sets$first
-        s2 <- sets$second      
-
-        # Computing the correction vector.
-        correction.in <- compute.correction.vectors(ref.batch.in, other.batch.in, s1, s2, other.batch.in.untrans, sigma)
-        if (!same.set) {
-            correction.out <- compute.correction.vectors(ref.batch.out, other.batch.out, s1, s2, other.batch.in.untrans, sigma)
-            # NOTE: use of 'other.batch.in.untrans' here is intentional, 
-            # as the distances should be the same as the MNN distances.
-        }
-
-        # Calculating the smallest angle between each correction vector and the first 2 basis vectors of the reference batch.
-        if (compute.angle) {
-            ref.centred <- t(ref.batch.in)
-            ref.centred <- ref.centred - rowMeans(ref.centred) 
-            ref.basis <- .svd_internal(ref.centred, nu=2, nv=0, pc.approx=pc.approx, irlba.args=irlba.args)$u
-
-            angle.out <- numeric(nrow(correction.in))
-            for (i in seq_along(angle.out)) {
-                angle.out[i] <- find.shared.subspace(ref.basis, t(correction.in[i,,drop=FALSE]))$angle
-            }
-            angle.list[[target]] <- angle.out
-        }
-
-        # Removing any component of the correction vector that's parallel to the biological basis vectors in either batch.
-        # Only using cells involved in MNN pairs, to avoid undercorrection in directions that weren't problematic anyway.
-        # (This is intended to mitigate the effect of identifying MNN pairs between non-corresponding populations.)
-        if (svd.dim>0) {
-            u1 <- unique(s1)
-            u2 <- unique(s2)
-
-            # Computing the biological subspace in both batches.
-            in.span1 <- get.bio.span(t(ref.batch.in[u1,,drop=FALSE]), ndim=svd.dim,
-                                     pc.approx=pc.approx, irlba.args=irlba.args)
-            in.span2 <- get.bio.span(other.batch.in.untrans[,u2,drop=FALSE], ndim=svd.dim, 
-                                     pc.approx=pc.approx, irlba.args=irlba.args)
-
-            # Reduce the component in each span from the batch correction vector.
-            correction.in <- subtract.bio(correction.in, in.span1, in.span2)
-
-            # Repeating for the output values.
-            if (!same.set) { 
-                out.span1 <- get.bio.span(t(ref.batch.out[u1,,drop=FALSE]), subset.row=subset.row,
-                                          ndim=svd.dim, pc.approx=pc.approx, irlba.args=irlba.args)
-                out.span2 <- get.bio.span(other.batch.out.untrans[,u2,drop=FALSE], subset.row=subset.row,
-                                          ndim=svd.dim, pc.approx=pc.approx, irlba.args=irlba.args)
-                correction.out <- subtract.bio(correction.out, out.span1, out.span2, subset.row=subset.row)
-            }
-        } 
-       
-        # Adjusting the shift variance; done after any SVD so that variance along the correction vector is purely technical.
-        if (var.adj) { 
-            correction.in <- adjust.shift.variance(ref.batch.in, other.batch.in, correction.in)
-            if (!same.set) {
-                correction.out <- adjust.shift.variance(ref.batch.out, other.batch.out, correction.out, subset.row=subset.row) 
-            }
-        }
-
-        # Applying the correction and expanding the reference batch. 
-        other.batch.in <- other.batch.in + correction.in
-        ref.batch.in <- rbind(ref.batch.in, other.batch.in)
-        if (same.set) {
-            output[[target]] <- t(other.batch.in)
-        } else {
-            other.batch.out <- other.batch.out + correction.out
-            ref.batch.out <- rbind(ref.batch.out, other.batch.out)
-            output[[target]] <- t(other.batch.out)
-        }
-
-        # Storing the identities of the MNN pairs (using RLEs for compression of runs).
-        mnn.list[[target]] <- DataFrame(current.cell=s2, other.cell=Rle(s1), other.batch=Rle(original.batch[s1]))
-        original.batch <- c(original.batch, rep(target, nrow(other.batch.in)))
-    }
-
-    # Formatting output to be consistent with input.
-    names(output) <- names(batches)
-    names(mnn.list) <- names(batches)
-    list(corrected=output, pairs=mnn.list)
+    return(list(In=in.batches, Out=out.batches, Subset=subset.row, Same=same.set))
 }
 
 find.mutual.nn <- function(data1, data2, k1, k2, BPPARAM) 
@@ -266,6 +278,7 @@ get.bio.span <- function(exprs, ndim, subset.row=NULL, pc.approx=FALSE, irlba.ar
 
     # Returning the basis vectors directly if there was no subsetting. 
     ndim <- min(ndim, dim(centred))
+    nv <- min(nv, ndim)
     S <- .svd_internal(centred, nu=ndim, nv=nv, pc.approx=pc.approx, irlba.args=irlba.args)
     if (is.null(subset.row)) { 
         return(S$u)
