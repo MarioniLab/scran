ae1fdefacc22a25a3300308cf99ca99d5f87fb6f
Aaron Lun <alun@wehi.edu.au>
2017-11-16 13:44:54 +0000

    Added new tests for buildKNNGraph. Moved more buildSNNGraph tests into test_that blocks.

--- tests/testthat/test-snn.R
+++ tests/testthat/test-snn.R
@@ -34,14 +34,16 @@ ncells <- 200
 ngenes <- 1000
 means <- 2^runif(ngenes, -1, 5)
 
-dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
-check(dummy, k=10)
-
-dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
-check(dummy, k=20)
-
-dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
-check(dummy, k=5)
+test_that("buildSNNGraph gives same results as a reference", {
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+    check(dummy, k=10)
+    
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+    check(dummy, k=20)
+    
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+    check(dummy, k=5)
+})
 
 # Checking that the value is sensible with subset.row.
 
@@ -50,14 +52,26 @@ are_graphs_same <- function(g1, g2) {
     return(TRUE)
 }
 
-selected <- sample(ngenes, 50)
-g <- buildSNNGraph(dummy[selected,])
-g2 <- buildSNNGraph(dummy, subset.row=selected)
-are_graphs_same(g, g2)
+set.seed(20001)
+test_that("Subsetting does not change the result", {
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+
+    selected <- sample(ngenes, 50)
+    g <- buildSNNGraph(dummy[selected,])
+    g2 <- buildSNNGraph(dummy, subset.row=selected)
+    are_graphs_same(g, g2)
+
+    selected <- rbinom(ngenes, 1, 0.5)==1
+    g <- buildSNNGraph(dummy[selected,])
+    g2 <- buildSNNGraph(dummy, subset.row=selected)
+    are_graphs_same(g, g2)
+})
 
 # Checking SCESet construction.
 
+set.seed(20002)
 test_that("buildSNNGraph works properly on SingleCellExperiment objects", {
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
     sce <- SingleCellExperiment(list(counts=2^dummy, logcounts=dummy))
     g <- buildSNNGraph(sce)
     g2 <- buildSNNGraph(assay(sce, "logcounts"))
@@ -67,6 +81,7 @@ test_that("buildSNNGraph works properly on SingleCellExperiment objects", {
     g2 <- buildSNNGraph(assay(sce, "counts"))
     are_graphs_same(g, g2)
     
+    selected <- sample(ngenes, 50)
     g <- buildSNNGraph(sce, subset.row=selected)
     g2 <- buildSNNGraph(sce[selected,])
     are_graphs_same(g, g2)
@@ -79,23 +94,27 @@ test_that("buildSNNGraph works properly on SingleCellExperiment objects", {
 
 # Checking multi-core processing works.
 
-dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
-ref <- scran:::.find_knn(dummy, k=10, BPPARAM=SerialParam())
-alt <- scran:::.find_knn(dummy, k=10, BPPARAM=SerialParam(), force=TRUE)
-expect_equal(ref, alt)
-
-dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
-ref <- scran:::.find_knn(dummy, k=5, BPPARAM=SerialParam())
-alt <- scran:::.find_knn(dummy, k=5, BPPARAM=SerialParam(), force=TRUE)
-expect_equal(ref, alt)
-
-# ref <- scran:::.find_knn(dummy, k=5)
-# alt <- scran:::.find_knn(dummy, k=5, BPPARAM=MulticoreParam(3))
-# expect_equal(ref, alt)
+set.seed(20003)
+test_that("Multi-core KNN detection is correct", {
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+    ref <- scran:::.find_knn(dummy, k=10, BPPARAM=SerialParam())
+    alt <- scran:::.find_knn(dummy, k=10, BPPARAM=SerialParam(), force=TRUE)
+    expect_equal(ref, alt)
+    
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+    ref <- scran:::.find_knn(dummy, k=5, BPPARAM=SerialParam())
+    alt <- scran:::.find_knn(dummy, k=5, BPPARAM=SerialParam(), force=TRUE)
+    expect_equal(ref, alt)
+    
+#   ref <- scran:::.find_knn(dummy, k=5)
+#   alt <- scran:::.find_knn(dummy, k=5, BPPARAM=MulticoreParam(3))
+#   expect_equal(ref, alt)
+})
 
 # Checking PCA was working.
 
-test_that("buildSNNGRaph with PCA works correctly", {
+set.seed(20004)
+test_that("buildSNNGraph with PCA works correctly", {
     dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
     pc <- prcomp(t(dummy))
     ref <- buildSNNGraph(t(pc$x[,1:20]), k=10, d=NA)
@@ -125,20 +144,59 @@ test_that("buildSNNGRaph with PCA works correctly", {
     alt <- buildSNNGraph(X, use.dimred="PCA")
     are_graphs_same(ref, alt)
 
-    # Ignores spike-in and subset.row specifications (correctly).
-    isSpike(X, "ERCC") <- selected
+    # Unaffected by spike-in and subset.row specifications (correctly).
+    isSpike(X, "ERCC") <- sample(ngenes, 50)
     alt <- buildSNNGraph(X, use.dimred="PCA")
     are_graphs_same(ref, alt)
 
+    alt <- buildSNNGraph(X, use.dimred="PCA", subset.row=!isSpike(X))
+    are_graphs_same(ref, alt)
 })
 
 # Silly inputs.
 
-dummy <- matrix(rnorm(ngenes*20), ncol=20, nrow=ngenes)
-suppressWarnings(expect_error(buildSNNGraph(dummy[,0], d=NA), "cannot create empty graph with negative number of vertices"))
-suppressWarnings(expect_error(buildSNNGraph(dummy[0,], d=NA), "cannot create empty graph with negative number of vertices"))
-expect_warning(out <- buildSNNGraph(dummy, k=50, d=NA), "'k' set to the number of cells minus 1")
-are_graphs_same(out, buildSNNGraph(dummy, k=19))
+test_that("buildSNNGraph fails on silly inputs", {
+    dummy <- matrix(rnorm(ngenes*20), ncol=20, nrow=ngenes)
+    suppressWarnings(expect_error(buildSNNGraph(dummy[,0], d=NA), "cannot create empty graph with negative number of vertices"))
+    suppressWarnings(expect_error(buildSNNGraph(dummy[0,], d=NA), "cannot create empty graph with negative number of vertices"))
+    expect_warning(out <- buildSNNGraph(dummy, k=50, d=NA), "'k' set to the number of cells minus 1")
+    are_graphs_same(out, buildSNNGraph(dummy, k=19))
+})
+
+# Checking that buildKNNGraph also works.
+
+KMAKE <- function(dummy, k, directed=FALSE) { 
+    ncells <- ncol(dummy)
+    collated <- matrix(0, ncells, ncells)
+    for (cell in seq_len(ncells)) {
+        d2 <- colSums((dummy[,cell] - dummy)^2)
+        chosen <- setdiff(order(d2), cell)[seq_len(k)]
+        collated[cell,chosen] <- 1
+        if (!directed) { 
+            collated[chosen,cell] <- 1
+        }
+    }
+    return(as(collated, "dgCMatrix"))
+}
+
+test_that("buildKNNGraph works correctly", {
+    dummy <- matrix(rnorm(ngenes*ncells), ncol=ncells, nrow=ngenes)
+    g <- buildKNNGraph(dummy, k=10, d=NA)
+    expect_false(is.directed(g))
+    expect_equal(g[], KMAKE(dummy, k=10))
+
+    g <- buildKNNGraph(dummy, k=10, d=NA, directed=TRUE)
+    expect_true(is.directed(g))
+    expect_equal(g[], KMAKE(dummy, k=10, directed=TRUE))
+
+    g <- buildKNNGraph(dummy, k=20, d=NA)
+    expect_equal(g[], KMAKE(dummy, k=20))
+
+    pc.out <- prcomp(t(dummy), rank.=10)
+    g <- buildKNNGraph(dummy, k=20, d=10)
+    expect_equal(g[], KMAKE(t(pc.out$x), k=20))
+})
+
 
 # Avoid normalize() overwriting scater's normalize() in other files.
 
