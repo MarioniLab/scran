596c8e594db5e9b5d68b4b5cf3852a83bacc4ec8
Aaron Lun <alun@wehi.edu.au>
2017-12-14 13:15:16 +0000

    Modified decomposeVar to support block and design.

--- R/decomposeVar.R
+++ R/decomposeVar.R
@@ -1,31 +1,57 @@
-.decompose_var <- function(x, fit, design=NA, subset.row=NULL, ...)
+.decompose_var <- function(x, fit, block=NA, design=NA, subset.row=NULL, ...)
 # Computes the biological variability of the log-CPMs by subtracting the
 # inferred technical variance from the total variance.
 #
 # written by Aaron Lun
 # created 21 January 2016 
-# last modified 11 July 2017
-{
-    if (!missing(x)) { 
-        subset.row <- .subset_to_index(subset.row, x, byrow=TRUE)
-        checked <- .make_var_defaults(x, fit=fit, design=design)
-        design <- checked$design
-        QR <- .ranksafe_qr(design)
-        
-        lout <- .Call(cxx_fit_linear_model, QR$qr, QR$qraux, x, subset.row-1L, FALSE)
-        lmeans <- lout[[1]]
-        lvar <- lout[[2]]
-        gnames <- rownames(x)[subset.row]
-    } else {
+{   
+    if (length(block)==1L && is.na(block)) {
+        block <- fit$block
+    }
+    if (length(design)==1L && is.na(design)) {
+        design <- fit$design
+    }
+
+    # Extracting statistics.
+    if (missing(x)) {
         lmeans <- fit$mean
         lvar <- fit$var
-        design <- fit$design
-        gnames <- names(lmeans)
+        resid.df <- fit$resid.df
+           
+    } else {
+        stats.out <- .get_var_stats(x, block=block, design=design, subset.row=subset.row)
+        lmeans <- stats.out$means
+        lvar <- stats.out$var
+        resid.df <- stats.out$resid.df
     }
-    
-    tech.var <- fit$trend(lmeans)
+
+    if (!is.null(block)) {
+        gnames <- rownames(lmeans)
+
+        # Need to do some work to calculate the technical component separately for each block.
+        block.tech.var <- fit$trend(as.vector(lmeans))
+        block.pval <- testVar(as.vector(lvar), null=block.tech.var, df=as.vector(resid.df), second.df=fit$df2, ...)
+        dim(block.pval) <- dim(block.tech.var) <- dim(lmeans)
+
+        # Aggregating variances together, using the weighted mean of residual d.f.
+        total.resid.df <- rowSums(resid.df)
+        lvar <- rowSums(lvar * resid.df)/total.resid.df
+        tech.var <- rowSums(block.tech.var * resid.df)/total.resid.df
+        
+        # Aggregating means together (effectively rowMeans(x), without having to recompute it).
+        num.per.block <- table(block)
+        lmeans <- lmeans %*% num.per.block[colnames(lmeans)]/ncol(x)
+
+        # Combining p-values using Fisher's method (independent observations).
+        logp <- -2*rowSums(log(block.pval))
+        pval <- pchisq(logp, df=2*ncol(block.pval), lower.tail=FALSE)
+    } else {
+        gnames <- names(lmeans)
+        tech.var <- fit$trend(lmeans)
+        pval <- testVar(total=lvar, null=tech.var, df=resid.df, second.df=fit$df2, ...)
+    }
+
     bio.var <- lvar - tech.var
-    pval <- testVar(total=lvar, null=tech.var, df=nrow(design) - ncol(design), second.df=fit$df2, ...)
     out <- data.frame(mean=lmeans, total=lvar, bio=bio.var, tech=tech.var,
                       p.value=pval, FDR=p.adjust(pval, method="BH"),
                       row.names=gnames)
@@ -38,6 +64,7 @@ setMethod("decomposeVar", c("ANY", "list"), .decompose_var)
 
 setMethod("decomposeVar", c("SingleCellExperiment", "list"), 
           function(x, fit, subset.row=NULL, ..., assay.type="logcounts", get.spikes=NA) {
+              
     # Checking whether we want to retrieve spikes but not use them in FDR calculations.
     if (is.na(get.spikes)) { 
         get.spikes <- TRUE
