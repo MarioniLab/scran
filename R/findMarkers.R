ecdfe3f0e6d7447c50760bfaae2272daaa00578c
Aaron Lun <alun@wehi.edu.au>
2018-01-05 10:34:03 +0000

    Fixed bug with log-fold change threshold tests in findMarkers. Streamlined internal functions to reduce redundancy. Exploited test symmetry in linear model version.

--- R/findMarkers.R
+++ R/findMarkers.R
@@ -17,29 +17,38 @@
 
     # Estimating the parameters.
     if (!is.null(block) || is.null(design)) {
-        fit <- .test_block_internal(x, subset.row, clusters, block=block, direction=direction, lfc=lfc, log.p=log.p)
+        fit <- .test_block_internal(x, subset.row, clusters, block=block, direction=direction, lfc=lfc)
     } else {
-        fit <- .fit_lm_internal(x, subset.row, clusters, design=design, direction=direction, lfc=lfc, log.p=log.p) 
+        fit <- .fit_lm_internal(x, subset.row, clusters, design=design, direction=direction, lfc=lfc)
     }
 
     # Figuring out how to rank within each DataFrame.
-    all.p <- fit$p.value
+    all.log.pval <- fit$p.value
     all.lfc <- fit$logFC
-    output <- vector("list", length(all.p))
-    names(output) <- names(all.p)
+    output <- vector("list", length(all.lfc))
+    names(output) <- names(all.lfc)
 
-    for (host in names(all.p)) {
-        pval <- .combine_pvalues(all.p[[host]], pval.type, log.p=log.p)
+    for (host in names(output)) {
         lfc <- all.lfc[[host]]
+        log.p.val <- all.log.pval[[host]]
         colnames(lfc) <- sprintf("logFC.%s", colnames(lfc))
 
         if (pval.type=="any") {
-            rank.out <- .rank_top_genes(all.p[[host]])
+            # Computing the Simes p-value (with NA protection).
+            pval <- .Call(cxx_combine_simes, log.p.val, TRUE)
+            if (!log.p) { pval <- exp(pval) }
+
+            # ... but ranking by position within each gene list.
+            rank.out <- .rank_top_genes(log.p.val)
             min.rank <- rank.out$rank
             min.p <- rank.out$value
             gene.order <- order(min.rank, min.p)
             preamble <- DataFrame(Top=min.rank)
         } else {
+            # Computing the IUT p-value.
+            pval <- log.p.val[.find_largest_col(log.p.val)]
+            if (!log.p) { pval <- exp(pval) }
+
             gene.order <- order(pval)
             preamble <- DataFrame(IUT.p=pval)
         }
@@ -68,7 +77,7 @@
 # Internal functions (blocking)
 ###########################################################
 
-.test_block_internal <- function(x, subset.row, clusters, block=NULL, direction="any", lfc=0, log.p=FALSE) 
+.test_block_internal <- function(x, subset.row, clusters, block=NULL, direction="any", lfc=0) 
 # This looks at every level of the blocking factor and performs
 # t-tests between pairs of clusters within each blocking level.
 {
@@ -120,11 +129,7 @@
     nblocks <- length(by.block)
     for (i in seq_along(clust.vals)) {
         host <- clust.vals[i]
-        if (lfc==0 || direction=="any") { 
-            targets <- clust.vals[seq_len(i-1L)]
-        } else {
-            targets <- clust.vals[-i]
-        }
+        targets <- clust.vals[seq_len(i-1L)]
 
         for (target in targets) {
             all.lfc <- matrix(0, ngenes, nblocks)
@@ -133,42 +138,41 @@
 
             # Performing the same pairwise t-test across all blocks.
             for (b in seq_len(nblocks)) { 
-                cur.lfc <- out.means[[b]][,host] - out.means[[b]][,target]
-                t.out <- .get_t_test_values(host.s2=out.s2[[b]][,host], target.s2=out.s2[[b]][,target],
-                                            host.n=out.n[[b]][host], target.n=out.n[[b]][target])
+                t.out <- .get_t_test_stats(host.s2=out.s2[[b]][,host], target.s2=out.s2[[b]][,target],
+                                           host.n=out.n[[b]][host], target.n=out.n[[b]][target])
+                all.rss <- all.rss + t.out$rss 
+                all.df <- all.df + t.out$residual.df
+                
                 cur.err <- t.out$err
                 cur.df <- t.out$test.df
-                cur.t <- .adjust_test_lfc(cur.lfc, lfc, direction=direction)/sqrt(cur.err)
+                cur.lfc <- out.means[[b]][,host] - out.means[[b]][,target]
+                p.out <- .run_t_test(cur.lfc, cur.err, cur.df, thresh.lfc=lfc, direction=direction)
 
                 all.lfc[,b] <- cur.lfc
                 all.weight[,b] <- 1/cur.err
-                all.left[,b] <- pt(cur.t, df=cur.df, lower.tail=TRUE, log.p=TRUE)
-                all.right[,b] <- pt(cur.t, df=cur.df, lower.tail=FALSE, log.p=TRUE)
-
-                all.rss <- all.rss + t.out$rss 
-                all.df <- all.df + t.out$residual.df
+                all.left[,b] <- p.out$left
+                all.right[,b] <- p.out$right
             }
 
             # Combining the p-values and log-fold changes across blocks.
-            com.p <- .combine_stouffer(all.left, all.right, all.weight, direction=direction, log.p=log.p)
-            com.lfc <- .combine_lfc(all.lfc, all.weight, all.rss/all.df)
-            out.p[[host]][,target] <- com.p
-            out.lfc[[host]][,target] <- com.lfc
+            stouffer.out <- .combine_stouffer(all.left, all.right, all.weight)
+            com.left <- stouffer.out$left
+            com.right <- stouffer.out$right
 
-            # Filling in the other values due to symmetry, except when lfc!=0; this requires explicit recalculation of 'cur.t'. 
+            # Flipping left/right to get the p-value from the reversed comparison.
+            out.p[[host]][,target] <- .choose_leftright_pvalues(com.left, com.right, direction=direction) 
+            out.p[[target]][,host] <- .choose_leftright_pvalues(com.right, com.left, direction=direction)
+
+            # Symmetrical log-fold changes.
+            com.lfc <- .combine_lfc(all.lfc, all.weight, all.rss/all.df)
+            out.lfc[[host]][,target] <- com.lfc
             out.lfc[[target]][,host] <- -com.lfc
-            if (direction=="any") { 
-                out.p[[target]][,host] <- com.p
-            } else if (lfc==0) {
-                symm.p <- .combine_stouffer(all.right, all.left, all.weight, direction=direction, log.p=log.p)
-                out.p[[target]][,host] <- symm.p
-            }
         }
     }
     return(list(p.value=out.p, logFC=out.lfc))    
 }
 
-.get_t_test_values <- function(host.s2, target.s2, host.n, target.n) 
+.get_t_test_stats <- function(host.s2, target.s2, host.n, target.n) 
 # Computes the error variance of the log-fold change and the 
 # degrees of freedom for the t-distribution.
 {
@@ -217,7 +221,7 @@
 # This computes the weighted average log-fold change across all batches.
 # Weighting is usually based on the squared error of the log-fold change,
 # so some finesse is required to use information from batches with n=1
-# for all groups.
+# for all groups - namely, by using the average variance 's2' as the known SD.
 {
     if (length(weights) && is.na(max(weights))) {
         s2[is.na(s2)] <- 1 # effectively ensures equal weights, if no one has any residual d.f.
@@ -231,46 +235,31 @@
     rowSums(weights*lfc, na.rm=TRUE)/rowSums(weights, na.rm=TRUE)
 }
 
-.combine_stouffer <- function(left, right, weights, direction="any", log.p=FALSE) 
+.combine_stouffer <- function(left, right, weights) 
 # This uses the inverse errors as the weights to combine the 
 # various statistics. It uses Stouffer's method to combine 
-# the p-values on either side.
+# the one-sided p-values, with weights for each column.
 {
     sum.weight <- rowSums(weights, na.rm=TRUE)
-    if (direction!="up") { 
-        p.left <- .run_stouffer(left, weights, sum.weight, log.p=log.p)
-    } else {
-        p.left <- NULL
-    }
-
-    if (direction!="down") { 
-        p.right <- .run_stouffer(right, weights, sum.weight, log.p=log.p)
-    } else {
-        p.right <- NULL
-    }
-
-    .combine_leftright_pvalues(p.left, p.right, direction=direction, log.p=log.p)
+    p.left <- .run_stouffer(left, weights, sum.weight)
+    p.right <- .run_stouffer(right, weights, sum.weight)
+    return(list(left=p.left, right=p.right))
 }
 
-.run_stouffer <- function(log.pvals, weights, sum.weight, log.p=FALSE) {
-    if (ncol(log.pvals)>1L) {
-        Z <- qnorm(log.pvals, log.p=TRUE)
-        final <- rowSums(Z*weights, na.rm=TRUE)/sum.weight
-        P <- pnorm(final, log.p=log.p)
-    } else {
-        P <- log.pvals
-        if (!log.p) {
-            P <- exp(log.pvals)
-        }
+.run_stouffer <- function(log.pvals, weights, sum.weight) {
+    if (ncol(log.pvals)==1L) {
+        return(log.pvals)
     }
-    return(P)
+    Z <- qnorm(log.pvals, log.p=TRUE)
+    final <- rowSums(Z*weights, na.rm=TRUE)/sum.weight
+    pnorm(final, log.p=TRUE)
 }
 
 ###########################################################
 # Internal functions (linear modelling)
 ###########################################################
 
-.fit_lm_internal <- function(x, subset.row, clusters, design, direction="any", lfc=0, log.p=FALSE) 
+.fit_lm_internal <- function(x, subset.row, clusters, design, direction="any", lfc=0) 
 # This fits a linear model to each gene and performs a t-test for 
 # differential expression between clusters.
 {
@@ -298,49 +287,46 @@
     stats <- .Call(cxx_fit_linear_model, QR$qr, QR$qraux, x, subset.row - 1L, TRUE)
     coefficients <- stats[[1]]
     coefficients[QR$pivot,] <- coefficients
-    # means <- stats[[2]] # Not actually needed here anymore.
     sigma2 <- stats[[3]]
 
     # Setting up output containers.
     out.lfc <- vector("list", length(clust.vals))
     names(out.lfc) <- clust.vals  
     out.p <- out.lfc
+    ngenes <- length(subset.row)
+    for (i in seq_along(clust.vals)) {
+        targets <- clust.vals[-i]
+        collected <- matrix(0, ngenes, length(targets))
+        colnames(collected) <- targets
+        host <- clust.vals[i]
+        out.p[[host]] <- out.lfc[[host]] <- collected
+    }
 
     # Doing a dummy fit, to avoid having to manually calculate standard errors.
     lfit <- lmFit(rbind(seq_len(nrow(full.design))), full.design)
     for (h in seq_along(clust.vals)) { 
         host <- clust.vals[h]
-        not.h <- seq_along(clust.vals)[-h]
-        targets <- clust.vals[not.h]
         
         # Computing standard errors (via limma).
-        con <- matrix(0, ncol(full.design), length(clust.vals))
+        con <- matrix(0, ncol(full.design), h-1L)
         diag(con) <- -1
         con[h,] <- 1
-        con <- con[,not.h,drop=FALSE]
         lfit2 <- contrasts.fit(lfit, con)
 
         # Computing log-fold changes, t-statistics and p-values on a per-contrast basis.
         # This _could_ be vectorised, but it's less confusing to do it like this,
         # and there's not much speed gain to be had from vectorizing over contrasts.
-        ngenes <- length(subset.row)
-        ncon <- length(not.h)
-        all.lfc <- all.p <- matrix(0, ngenes, ncon)
         ref.coef <- coefficients[h,]
+        for (tdex in seq_len(h-1L)) { 
+            target <- clust.vals[tdex]
+            cur.lfc <- ref.coef - coefficients[tdex,]
+            out.lfc[[host]][,target] <- cur.lfc
+            out.lfc[[target]][,host] <- -cur.lfc
 
-        for (con in seq_len(ncon)) { 
-            cur.lfc <- ref.coef - coefficients[not.h[con],]
-            all.lfc[,con] <- cur.lfc
-            cur.t <- .adjust_test_lfc(cur.lfc, lfc, direction=direction)/(lfit2$stdev.unscaled[con]*sqrt(sigma2))
-
-            left.p <- pt(cur.t, df=resid.df, log.p=log.p, lower.tail=TRUE)
-            right.p <- pt(cur.t, df=resid.df, log.p=log.p, lower.tail=FALSE)
-            all.p[,con] <- .combine_leftright_pvalues(left.p, right.p, direction=direction, log.p=log.p)
+            test.out <- .run_t_test(cur.lfc, lfit2$stdev.unscaled[tdex]*sqrt(sigma2), resid.df, thresh.lfc=lfc, direction=direction)
+            out.p[[host]][,target] <- .choose_leftright_pvalues(test.out$left, test.out$right, direction=direction)
+            out.p[[target]][,host] <- .choose_leftright_pvalues(test.out$right, test.out$left, direction=direction)
         }
-
-        colnames(all.p) <- colnames(all.lfc) <- targets
-        out.p[[host]] <- all.p
-        out.lfc[[host]] <- all.lfc
     }
     return(list(p.value=out.p, logFC=out.lfc))
 }
@@ -349,59 +335,65 @@
 # Internal functions (p-value calculations)
 ###########################################################
 
-.adjust_test_lfc <- function(cur.lfc, thresh.lfc, direction="any")
-# Adjusting the log-FC for the proposed test threshold. The logic
-# is that the null log-fold changes are distributed on the closer
-# boundary, if direction="any"; and on the corresponding positive/
-# negative boundary for other values of 'direction'.
+.run_t_test <- function(cur.lfc, cur.err, cur.df, thresh.lfc=0, direction="any") 
+# This runs the t-test given the relevant statistics. We use the TREAT method
+# when thresh.lfc!=0, which tests against a null where the log-fold change is
+# takes values at the extremes of [-thresh, thresh]. This is 50% distributed
+# across both extremes, hence the -log(2) at the end.
 {
-    if (thresh.lfc==0) {
-        return(cur.lfc)
-    }
     thresh.lfc <- abs(thresh.lfc)
-    if (direction=="any") {
-        adj <- sign(cur.lfc) * pmin(thresh.lfc, abs(cur.lfc))
-    } else if (direction=="up") {
-        adj <- thresh.lfc
-    } else if (direction=="down") {
-        adj <- -thresh.lfc
+    if (thresh.lfc==0) {    
+        cur.t <- cur.lfc/sqrt(cur.err)
+        left <- pt(cur.t, df=cur.df, lower.tail=TRUE, log.p=TRUE)
+        right <- pt(cur.t, df=cur.df, lower.tail=FALSE, log.p=TRUE)
+    } else {
+        upper.t <- (cur.lfc - thresh.lfc)/cur.err
+        lower.t <- (cur.lfc + thresh.lfc)/cur.err
+
+        if (direction=="any") { 
+            left <- .add_log_values(pt(upper.t, df=cur.df, lower.tail=TRUE, log.p=TRUE),
+                                    pt(lower.t, df=cur.df, lower.tail=TRUE, log.p=TRUE)) - log(2)
+            right <- .add_log_values(pt(upper.t, df=cur.df, lower.tail=FALSE, log.p=TRUE),
+                                     pt(lower.t, df=cur.df, lower.tail=FALSE, log.p=TRUE)) - log(2)
+        } else {
+            # Note that if direction='up', only 'right' is used; nonetheless, 'left' is still calculated
+            # using 'lower.t' so as to allow quick calculation of the p-value for the reversed contrast,
+            # by simply swapping 'left' and 'right'. The same applies when direction='down'.
+            left <- pt(lower.t, df=cur.df, lower.tail=TRUE, log.p=TRUE)
+            right <- pt(upper.t, df=cur.df, lower.tail=FALSE, log.p=TRUE) 
+        }
     }
-    cur.lfc - adj
+    return(list(left=left, right=right))
 }
 
-.combine_leftright_pvalues <- function(left, right, direction="any", log.p=FALSE) {
+.add_log_values <- function(x, y) 
+# This performs log(exp(x) + exp(y)) in a numerically
+# stable fashion, i.e., without actually running the 'exp's.
+{
+    pmax(x, y) + log(1 + exp(-abs(x-y)))
+}
+
+.choose_leftright_pvalues <- function(left, right, direction="any") 
+# Choosing whether to use the p-values from the left (lower.tail), or 
+# the p-values from the right (upper.tail), or to make it two-sided.
+# Assumes 'left' and 'right' are log-transformed p-values.    
+{
     if (direction=="up") { 
         return(right)
     } else if (direction=="down") {
         return(left)
-    } else if (direction=="any") {
-        p.out <- pmin(left, right)
-        if (log.p) {
-            p.out <- p.out + log(2)
-        } else {
-            p.out <- p.out * 2            
-        }
-        return(p.out)
-    }
-}
-
-.combine_pvalues <- function(all.p, pval.type, log.p=FALSE) { 
-    ngenes <- nrow(all.p)
-    ncon <- ncol(all.p)
-    if (pval.type=="any") { 
-        # Computing the Simes p-value (with NA protection).
-        pval <- .Call(cxx_combine_simes, all.p, log.p)
     } else {
-        # Computing the IUT p-value.
-        pval <- all.p[.find_largest_col(all.p)]
+        log.p.out <- pmin(left, right) + log(2)
+        return(log.p.out)
     }
-
-    return(pval)
 }
 
-.logBH <- function(pval) {
-    o <- order(pval)
-    repval <- pval[o] + log(length(o)/seq_along(o))
+.logBH <- function(log.p.val) 
+# Same as log(p.adjust(exp(log.p.val), method="BH")), without
+# the need to undo and redo the log-transformations.
+{
+    o <- order(log.p.val)
+    repval <- log.p.val[o] + log(length(o)/seq_along(o))
     repval <- rev(cummin(rev(repval)))
     repval[o] <- repval
     return(repval)
