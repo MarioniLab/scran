ece6d68cbb3336de0923b735e0b1f41d06166323
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-03 16:22:13 +0100

    Modified MNN C++, R code to support sparse matrices.

--- src/find_mutual_nns.cpp
+++ src/find_mutual_nns.cpp
@@ -103,48 +103,51 @@ SEXP cosine_norm(SEXP incoming, SEXP getmat) {
 
 SEXP smooth_gaussian_kernel(SEXP vect, SEXP index, SEXP data, SEXP sigma) {
     BEGIN_RCPP
-    const Rcpp::NumericMatrix correction_vectors(vect);
-    const int npairs=correction_vectors.nrow();
-    const int ngenes=correction_vectors.ncol();
+    auto correction_vectors=beachmat::create_numeric_matrix(vect);
+    const size_t npairs=correction_vectors->get_nrow();
+    const size_t ngenes=correction_vectors->get_ncol();
     const Rcpp::IntegerVector _index(index);
     if (npairs!=_index.size()) { 
         throw std::runtime_error("number of rows in 'vect' should be equal to length of 'index'");
     }
     
     // Constructing the average vector for each MNN cell.
-    std::deque<std::vector<double> > averages;
-    std::deque<int> number;
+    std::deque<Rcpp::NumericVector> averages;
     std::set<int> mnncell;
-
-    int row=0;
-    for (const auto& i : _index) {
-        auto currow=correction_vectors.row(row);
-        ++row;
-        
-        if (i >= averages.size() || averages[i].empty()) { 
-            if (i >= averages.size()) { 
-                averages.resize(i+1);
-                number.resize(i+1);
+    {
+        std::deque<int> number;
+        Rcpp::NumericVector currow(ngenes);
+        int row=0;
+        for (const auto& i : _index) {
+            correction_vectors->get_row(row, currow.begin());
+            ++row;
+            
+            if (i >= averages.size() || averages[i].size()==0) { 
+                if (i >= averages.size()) { 
+                    averages.resize(i+1);
+                    number.resize(i+1);
+                }
+                averages[i]=currow;
+                number[i]=1;
+                mnncell.insert(i);
+                currow=Rcpp::NumericVector(ngenes);
+            } else {
+                auto& target=averages[i];
+                auto cIt=currow.begin();
+                for (auto& t : target){
+                    t += *cIt;
+                    ++cIt;
+                }
+                ++(number[i]);
             }
-            averages[i]=std::vector<double>(currow.begin(), currow.end());
-            number[i]=1;
-            mnncell.insert(i);
-        } else {
-            auto& target=averages[i];
-            auto cIt=currow.begin();
-            for (auto& t : target){
-                t += *cIt;
-                ++cIt;
-            }
-            ++(number[i]);
         }
-    }
-
-    for (const auto& i : mnncell) {
-        auto& target=averages[i];
-        const auto& num=number[i];
-        for (auto& t : target) {
-            t/=num;
+    
+        for (const auto& i : mnncell) {
+            auto& target=averages[i];
+            const auto& num=number[i];
+            for (auto& t : target) {
+                t/=num;
+            }
         }
     }
 
@@ -227,9 +230,9 @@ SEXP smooth_gaussian_kernel(SEXP vect, SEXP index, SEXP data, SEXP sigma) {
 
 /* Perform variance adjustment with weighted distributions */
 
-double sq_distance_to_line(Rcpp::NumericMatrix::Column::const_iterator ref, 
-                           std::vector<double>::const_iterator grad,
-                           Rcpp::NumericMatrix::Column::const_iterator point,
+double sq_distance_to_line(const double* ref, 
+                           const double* grad,
+                           const double* point,
                            std::vector<double>& working) {
     for (auto& w : working) { // Calculating the vector difference from "point" to "ref".
         w=*ref - *point;
@@ -251,40 +254,45 @@ double sq_distance_to_line(Rcpp::NumericMatrix::Column::const_iterator ref,
 
 SEXP adjust_shift_variance(SEXP data1, SEXP data2, SEXP vect, SEXP sigma) {
     BEGIN_RCPP
-    const Rcpp::NumericMatrix _data1(data1), _data2(data2), _vect(vect);
-    const size_t ngenes=_data1.nrow();
-    if (ngenes!=_data2.nrow() || ngenes!=_vect.ncol()) { 
+    auto d1=beachmat::create_numeric_matrix(data1);
+    auto d2=beachmat::create_numeric_matrix(data2);
+    auto v=beachmat::create_numeric_matrix(vect);
+    const size_t ngenes=d1->get_nrow();
+    if (ngenes!=d2->get_nrow() || ngenes!=v->get_ncol()) { 
         throw std::runtime_error("number of genes do not match up between matrices");
     }
-    const size_t ncells1=_data1.ncol(), ncells2=_data2.ncol();
-    if (ncells2!=_vect.nrow()) {
+    const size_t ncells1=d1->get_ncol(), ncells2=d2->get_ncol();
+    if (ncells2!=v->get_nrow()) {
         throw std::runtime_error("number of cells do not match up between matrices");
     }        
     const double s2=check_numeric_scalar(sigma, "sigma");
 
-    std::vector<double> grad(ngenes), working(ngenes);
+    std::vector<double> working(ngenes);
     std::vector<std::pair<double, double> > distance1(ncells1); 
     Rcpp::NumericVector output(ncells2);
 
+    // Temporary objects for beachmat extraction.
+    Rcpp::NumericVector grad(ngenes), 
+        tmpcell_current(ngenes), 
+        tmpcell_same(ngenes),
+        tmpcell_other(ngenes);
+
     // Iterating through all cells.
     for (size_t cell=0; cell<ncells2; ++cell) {
-        const auto curcell=_data2.column(cell);
+        const auto curcell=d2->get_const_col(cell, tmpcell_current.begin());
 
         // Calculating the l2 norm and adjusting to a unit vector.
         double l2norm=0;
-        {
-            const auto curvect=_vect.row(cell);
-            std::copy(curvect.begin(), curvect.end(), grad.begin());
-            for (const auto& g : grad) {
-                l2norm+=g*g;            
-            }
-            l2norm=std::sqrt(l2norm);
-            for (auto& g : grad) { 
-                g/=l2norm;
-            }
+        v->get_row(cell, grad.begin());
+        for (const auto& g : grad) {
+            l2norm+=g*g;            
+        }
+        l2norm=std::sqrt(l2norm);
+        for (auto& g : grad) { 
+            g/=l2norm;
         }
                 
-        const double curproj=std::inner_product(grad.begin(), grad.end(), curcell.begin(), 0.0);
+        const double curproj=std::inner_product(grad.begin(), grad.end(), curcell, 0.0);
 
         // Getting the cumulative probability of each cell in its own batch.
         double prob2=0, totalprob2=0;    
@@ -293,9 +301,9 @@ SEXP adjust_shift_variance(SEXP data1, SEXP data2, SEXP vect, SEXP sigma) {
                 prob2+=1;
                 totalprob2+=1;
             } else {
-                const auto samecell=_data2.column(same);
-                const double sameproj=std::inner_product(grad.begin(), grad.end(), samecell.begin(), 0.0); // Projection
-                const double samedist=sq_distance_to_line(curcell.begin(), grad.begin(), samecell.begin(), working); // Distance.
+                const auto samecell=d2->get_const_col(same, tmpcell_same.begin());
+                const double sameproj=std::inner_product(grad.begin(), grad.end(), samecell, 0.0); // Projection
+                const double samedist=sq_distance_to_line(curcell, grad.begin(), samecell, working); // Distance.
                 
                 const double sameprob=std::exp(-samedist/s2);
                 if (sameproj <= curproj) {
@@ -309,9 +317,9 @@ SEXP adjust_shift_variance(SEXP data1, SEXP data2, SEXP vect, SEXP sigma) {
         // Filling up the coordinates and weights for the reference batch.
         double totalprob1=0;
         for (size_t other=0; other<ncells1; ++other) {
-            const auto othercell=_data1.column(other);
-            distance1[other].first=std::inner_product(grad.begin(), grad.end(), othercell.begin(), 0.0); // Projection
-            const double otherdist=sq_distance_to_line(curcell.begin(), grad.begin(), othercell.begin(), working); // Distance.
+            const auto othercell=d1->get_const_col(other, tmpcell_other.begin());
+            distance1[other].first=std::inner_product(grad.begin(), grad.end(), othercell, 0.0); // Projection
+            const double otherdist=sq_distance_to_line(curcell, grad.begin(), othercell, working); // Distance.
             totalprob1+=(distance1[other].second=std::exp(-otherdist/s2));
         }
         std::sort(distance1.begin(), distance1.end());
