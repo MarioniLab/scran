a7024484bd5ef772848799c95c7d5f42f237b3df
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-03 10:17:49 +0100

    Added S4 methods, extra cluster-related information to doublet output.

--- R/doubletCluster.R
+++ R/doubletCluster.R
@@ -1,7 +1,7 @@
 #' @importFrom scater librarySizeFactors normalize
-#' @importFrom BiocGenerics "sizeFactors<-"
+#' @importFrom BiocGenerics "sizeFactors<-" sizeFactors
 #' @importFrom stats p.adjust
-.doublet_cluster <- function(x, clusters, subset.row=NULL, threshold=0.05) 
+.doublet_cluster <- function(x, clusters, subset.row=NULL, threshold=0.05, ...) 
 # Finds evidence that a cluster is _not_ a doublet of two other clusters.
 # Absence of such evidence should be a warning flag for that cluster.
 # 
@@ -12,7 +12,10 @@
     sce <- SingleCellExperiment(list(counts=x))
     sizeFactors(sce) <- librarySizeFactors(x)
     sce <- normalize(sce, return_log=TRUE)
-    degs <- findMarkers(sce, clusters=clusters, subset.row=subset.row, full.stats=TRUE)
+
+    degs <- findMarkers(sce, clusters=clusters, subset.row=subset.row, full.stats=TRUE, ...)
+    med.lib.size <- lapply(split(sizeFactors(sce), clusters), median)
+	n.cluster <- table(clusters)/length(clusters)
 
     # Running through all pairs of clusters and testing against the third cluster.
     collected <- list()
@@ -48,18 +51,35 @@
             }
         }
 
-        print(best.N)
-        print(best.parents)
-        print(best.gene)
-        print(best.p)
-        collected[[ref]] <- DataFrame(N=best.N, 
-            Source1=remnants[best.parents[1]],
-            Source2=remnants[best.parents[2]],
-            best=rownames(ref.stats)[best.gene], 
-            p.value=best.p, row.names=ref)
+        parent1 <- remnants[best.parents[1]]
+        parent2 <- remnants[best.parents[2]]
+        collected[[ref]] <- DataFrame(N=best.N, source1=parent1, source2=parent2,
+            best=rownames(ref.stats)[best.gene], p.value=best.p, 
+			prop=n.cluster[[ref]],
+            lib.size1=med.lib.size[[parent1]]/med.lib.size[[ref]], 
+			lib.size2=med.lib.size[[parent2]]/med.lib.size[[ref]], 
+            row.names=ref)
     }
 
     # Returning the DataFrame of compiled results.
     out <- do.call(rbind, collected)
     out[order(out$N),]
 }
+
+##############################
+# S4 method definitions here #
+##############################
+
+#' @export
+setGeneric("doubletCluster", function(x, ...) standardGeneric("doubletCluster"))
+
+#' @export
+setMethod("doubletCluster", "ANY", .doublet_cluster)
+
+#' @importFrom SummarizedExperiment assay "assay<-"
+#' @importFrom SingleCellExperiment reducedDim isSpike
+#' @export
+setMethod("doubletCluster", "SingleCellExperiment", function(x, ..., subset.row=NULL, assay.type="logcounts", get.spikes=FALSE) {
+    subset.row <- .SCE_subset_genes(subset.row=subset.row, x=x, get.spikes=get.spikes)
+    .doublet_cluster(assay(x, i=assay.type), ..., subset.row=subset.row)
+})
