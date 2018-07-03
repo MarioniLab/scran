e9e15357e048c5cb043ba7df09f3006bc90eabd2
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-03 10:08:07 +0100

    Added automatic determination of batch order in fastMNN. Support parallelization in tricube step for NN finding. Added batch order, pairing information to fastMNN output.

--- R/fastMNN.R
+++ R/fastMNN.R
@@ -1,13 +1,12 @@
 #' @export
 #' @importFrom BiocParallel SerialParam
 fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE, 
-    irlba.args=list(), subset.row=NULL, BPPARAM=SerialParam()) 
+    irlba.args=list(), subset.row=NULL, auto.order=FALSE, BPPARAM=SerialParam()) 
 # A faster version of the MNN batch correction approach.
 # 
 # written by Aaron Lun
 # created 26 May 2018
 {
-
     batches <- list(...) 
     nbatches <- length(batches) 
     if (nbatches < 2L) { 
@@ -40,17 +39,40 @@ fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE,
     
     # Performing a multi-sample PCA.
     pc.mat <- .multi_pca(batches, d=d, approximate=approximate, use.crossprod=TRUE, irlba.args=irlba.args)
+    mnn.pairings <- vector("list", nbatches-1L)
 
-    refdata <- pc.mat[[1]]
     for (bdx in 2:nbatches) {
-        curdata <- pc.mat[[bdx]]
-        
-        # Finding MNNs between batches and obtaining an estimate of the overall batch vector.
-        mnn.sets <- find.mutual.nn(refdata, curdata, k1=k, k2=k, BPPARAM=BPPARAM)
+        if (auto.order) {
+            # Automatically choosing to merge batches with the largest number of MNNs.
+            if (bdx==2L) {
+                d.out <- .define_first_merge(pc.mat, k=k, BPPARAM=BPPARAM)
+                refdata <- pc.mat[[d.out$first]]
+                curdata <- pc.mat[[d.out$second]]
+                mnn.sets <- d.out$pairs
+                precomp <- d.out$precomputed
+                processed <- c(d.out$first, d.out$second)
+            } else {
+                d.out <- .define_next_merge(refdata, pc.mat, processed, precomp, k=k, BPPARAM=BPPARAM)
+                curdata <- pc.mat[[d.out$other]]
+                mnn.sets <- d.out$pairs
+                processed <- c(processed, d.out$other)
+            }
+        } else {
+            # Otherwise, using consecutive batches in the supplied order.
+            if (bdx==2L) {
+                refdata <- pc.mat[[1]]
+                processed <- 1L
+            }
+            curdata <- pc.mat[[bdx]]
+            mnn.sets <- find.mutual.nn(refdata, curdata, k1=k, k2=k, BPPARAM=BPPARAM)
+            processed <- c(processed, bdx)
+        }
+
+        # Estimate the overall batch vector.
         ave.out <- .average_correction(refdata, mnn.sets$first, curdata, mnn.sets$second)
         overall.batch <- colMeans(ave.out$averaged)
 
-        # Projecting along the batch vector, and shifting all cells to the center _within_ each batch.
+        # Remove variation along the batch vector.
         refdata <- .center_along_batch_vector(refdata, overall.batch)
         curdata <- .center_along_batch_vector(curdata, overall.batch)
 
@@ -58,23 +80,34 @@ fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE,
 #        re.mnn.sets <- find.mutual.nn(refdata, curdata, k1=k, k2=k, BPPARAM=BPPARAM)
 #        re.ave.out <- .average_correction(refdata, re.mnn.sets$first, curdata, re.mnn.sets$second)
 
-        # Recomputing the correction vectors after removing the within-batch variation.
+        # Recompute each correction vector, and use it for correction.
         re.ave.out <- .average_correction(refdata, mnn.sets$first, curdata, mnn.sets$second)
-        
-        curdata <- .tricube_weighted_correction(curdata, re.ave.out$averaged, re.ave.out$second, k=k, ndist=ndist)
+        curdata <- .tricube_weighted_correction(curdata, re.ave.out$averaged, re.ave.out$second, 
+            k=k, ndist=ndist, BPPARAM=BPPARAM)
+
         refdata <- rbind(refdata, curdata)
+        mnn.pairings[[bdx-1L]] <- DataFrame(mnn.sets)
     }
     
-    # Figuring out what output to return.
-    ncells <- vapply(batches, FUN=ncol, FUN.VALUE=0L)
+    # Characterizing the original order of the batches and cells.
     if (!is.null(names(batches))) {
-        batch.ids <- rep(names(batches), ncells)
+        batch.ids <- names(batches)
     } else {
-        batch.ids <- rep(seq_along(ncells), ncells)
+        batch.ids <- seq_along(batches)
     }
-    return(list(corrected=refdata, batch=batch.ids))
+    batch.ids <- batch.ids[processed]
+    ncells <- vapply(batches, FUN=ncol, FUN.VALUE=0L)[processed]
+
+    batch.ids <- rep(batch.ids, ncells)
+    cell.ids <- unlist(lapply(ncells, seq_len))
+    origin <- DataFrame(batch=batch.ids, cell=cell.ids)
+
+    return(list(corrected=refdata, origin=origin, pairs=mnn.pairings))
 }
  
+############################################
+# Fast PCA functions.
+
 #' @importFrom DelayedArray DelayedArray t
 #' @importFrom DelayedMatrixStats rowMeans2
 .multi_pca <- function(mat.list, d=50, approximate=FALSE, irlba.args=list(), use.crossprod=FALSE, BPPARAM=SerialParam()) 
@@ -240,6 +273,9 @@ fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE,
     return(output)
 }
 
+############################################
+# Correction-related functions.
+
 .average_correction <- function(refdata, mnn1, curdata, mnn2)
 # Computes correction vectors for each MNN pair, and then
 # averages them for each MNN-involved cell in the second batch.
@@ -253,6 +289,7 @@ fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE,
 }
 
 .center_along_batch_vector <- function(mat, batch.vec) 
+# Projecting along the batch vector, and shifting all cells to the center _within_ each batch.
 # This removes any variation along the overall batch vector within each matrix.
 {
     batch.vec <- batch.vec/sqrt(sum(batch.vec^2))
@@ -263,13 +300,14 @@ fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE,
 }
 
 #' @importFrom kmknn queryKNN
-.tricube_weighted_correction <- function(curdata, correction, in.mnn, k=20, ndist=3)
+#' @importFrom BiocParallel SerialParam
+.tricube_weighted_correction <- function(curdata, correction, in.mnn, k=20, ndist=3, BPPARAM=SerialParam())
 # Computing tricube-weighted correction vectors for individual cells,
 # using the nearest neighbouring cells _involved in MNN pairs_.
 {
     cur.uniq <- curdata[in.mnn,,drop=FALSE]
     safe.k <- min(k, nrow(cur.uniq))
-    closest <- queryKNN(query=curdata, X=cur.uniq, k=safe.k)
+    closest <- queryKNN(query=curdata, X=cur.uniq, k=safe.k, BPPARAM=BPPARAM)
 
     middle <- ceiling(safe.k/2L)
     mid.dist <- closest$distance[,middle]
@@ -284,3 +322,59 @@ fastMNN <- function(..., k=20, cos.norm=TRUE, d=50, ndist=3, approximate=FALSE,
     
     return(curdata)
 }
+
+############################################
+# Auto-ordering functions.
+
+#' @importFrom kmknn queryKNN
+#' @importFrom BiocParallel SerialParam
+.define_first_merge <- function(pc.mat, k=20, BPPARAM=SerialParam())
+# Find the pair of matrices in 'options' which has the greatest number of MNNs.
+{
+    precomputed <- lapply(pc.mat, precluster)
+    max.pairs <- list()
+
+    for (First in seq_along(precomputed)) {
+        fdata <- pc.mat[[First]]
+        for (Second in seq_len(First-1L)) {
+            sdata <- pc.mat[[Second]]
+
+            W21 <- queryKNN(precomputed=precomputed[[Second]], query=fdata, k=k, BPPARAM=BPPARAM, get.distance=FALSE)
+            W12 <- queryKNN(precomputed=precomputed[[First]], query=sdata, k=k, BPPARAM=BPPARAM, get.distance=FALSE)
+            out <- .Call(cxx_find_mutual_nns, W21$index, W12$index)
+            names(out) <- c("first", "second")
+
+            if (length(out$first) > length(max.pairs$first))  {
+                max.pairs <- out
+                max.first <- First
+                max.second <- Second
+            }
+        }
+    }
+
+    return(list(first=max.first, second=max.second, pairs=max.pairs, precomputed=precomputed))
+}
+
+#' @importFrom kmknn queryKNN
+#' @importFrom BiocParallel SerialParam
+.define_next_merge <- function(refdata, pc.mat, processed, precomputed, k=20, BPPARAM=SerialParam()) 
+# Find the matrix in pc.mat[-processed] that has the greater number of MNNs to 'refdata'.
+{
+    max.pairs <- list()
+    precomp.ref <- precluster(refdata)
+
+    for (other in seq_along(pc.mat)[-processed]) {
+        W21 <- queryKNN(precomputed=precomputed[[other]], query=refdata, k=k, BPPARAM=BPPARAM, get.distance=FALSE)
+        W12 <- queryKNN(precomputed=precomp.ref, query=pc.mat[[other]], k=k, BPPARAM=BPPARAM, get.distance=FALSE)
+        out <- .Call(cxx_find_mutual_nns, W21$index, W12$index)
+        names(out) <- c("first", "second")
+
+        if (length(out$first) > length(max.pairs$first))  {
+            max.pairs <- out
+            max.other <- other
+        }
+    }
+
+    return(list(other=max.other, pairs=max.pairs))
+}
+
