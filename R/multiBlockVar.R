7c50683f1a0d870c9768230b5a6b77fc71a257ae
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-04 17:25:13 +0100

    Moved multiBlockNorm to its own file.

--- R/multiBlockVar.R
+++ R/multiBlockVar.R
@@ -41,37 +41,3 @@ multiBlockVar <- function(x, block, trend.args=list(), dec.args=list(), assay.ty
     combined$per.block <- stored
     return(combined)
 }
-
-#' @export
-#' @importFrom scater librarySizeFactors
-#' @importFrom SingleCellExperiment sizeFactorNames
-#' @importFrom BiocGenerics sizeFactors sizeFactors<- normalize
-#' @importFrom stats ave
-multiBlockNorm <- function(x, block, ...) 
-# Adjusts size factors so that the spike-in size factors have
-# the same mean as the reference size factors _within each batch_.
-# Ensures that the abundances are comparable in multiBlockVar.
-#
-# written by Aaron Lun
-# created 4 June 2018
-{
-    ref <- sizeFactors(x)
-    if (is.null(ref)) {
-        ref <- librarySizeFactors(x)
-        warning("using library sizes as size factors")
-    } else {
-        # Centering just in case.
-        ref <- ref / mean(ref)
-        sizeFactors(x) <- ref
-    }
-    ref.mean <- ave(ref, block, FUN=mean)
-
-    for (sf in sizeFactorNames(x)) {
-        current <- sizeFactors(x, sf)
-        cur.mean <- ave(current, block, FUN=mean)
-        sizeFactors(x, sf) <- current * ref.mean / cur.mean
-    }
-   
-    x <- normalize(x, ..., centre_size_factors=FALSE)
-    return(x)
-}
