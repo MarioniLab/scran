071551d5c054ea522ef464ff049a581c7a6c4a22
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-05 10:13:07 +0100

    Restructured multiBatchNorm to be sensible with spike-ins.

--- R/multiBatchNorm.R
+++ R/multiBatchNorm.R
@@ -1,8 +1,7 @@
 #' @export
-#' @importFrom scater calcAverage
-#' @importFrom BiocGenerics normalize sizeFactors sizeFactors<-
-#' @importFrom stats median
-multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=1)
+#' @importFrom BiocGenerics normalize
+#' @importFrom SingleCellExperiment isSpike spikeNames
+multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=1, subset.row=NULL)
 # Performs multi-batch normalization, adjusting for differences 
 # in scale between SCE objects supplied in '...'.
 # 
@@ -10,11 +9,56 @@ multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=
 # created 4 June 2018
 {
     batches <- list(...)
-    nbatches <- length(batches)
-    .check_batch_consistency(batches, byrow=TRUE, ignore.null=TRUE)
+    .check_batch_consistency(batches, byrow=TRUE)
 
-    # Computing the median ratios (a la DESeq normalization).
-    collected.ave <- lapply(batches, calcAverage, use_size_factors=FALSE)
+    # Checking spikes.
+    ref.spike.names <- spikeNames(batches[[1]])
+    ref.spike <- isSpike(batches[[1]])
+    for (b in seq_along(batches)) {
+        if (!identical(ref.spike.names, spikeNames(batches[[b]]))) {
+            stop("spike-in sets differ across batches")
+        }
+        if (!identical(ref.spike, isSpike(batches[[b]]))) {
+            stop("spike-in identities differ across batches")
+        }
+    }
+
+    # Adjusting size factors for the non-spike-in transcripts.
+    nonspike.subset <- .SCE_subset_genes(subset.row, batches[[1]], get.spikes=FALSE)
+    if (is.null(nonspike.subset) || any(nonspike.subset)) { # works logical or integer
+        batches <- .batch_rescaler(batches, subset.row=nonspike.subset, exprs_values=assay.type, sf.type=NULL, min.mean=min.mean)
+    }
+
+    # Adjusting size factors for each of the spike-ins.
+    subset.row <- .subset_to_index(subset.row, batches[[1]], byrow=TRUE)
+
+    for (spike in ref.spike.names) {
+        ref.spike <- isSpike(batches[[1]], spike)
+        for (b in seq_along(batches)) {
+            if (!identical(ref.spike, isSpike(batches[[b]], spike))) {
+                stop(sprintf("%s spike-in identities differ across batches", spike))
+            }
+        }
+
+        spike.subset <- intersect(which(ref.spike), subset.row)
+        if (length(spike.subset)) { 
+            batches <- .batch_rescaler(batches, subset.row=spike.subset, exprs_values=assay.type, sf.type=spike, min.mean=min.mean)
+        }
+    }
+
+    # Applying the normalization.   
+    mapply(FUN=normalize, batches, MoreArgs=c(list(exprs_values=assay.type, centre_size_factors=FALSE), norm.args), SIMPLIFY=FALSE)
+}
+
+#' @importFrom scater calcAverage
+#' @importFrom stats median
+#' @importFrom BiocGenerics sizeFactors sizeFactors<-
+.batch_rescaler <- function(batches, subset.row, exprs_values, sf.type, min.mean) 
+# Computes the median ratios (a la DESeq normalization),
+# finds the smallest ratio and uses that as the reference.
+{
+    collected.ave <- lapply(batches, calcAverage, use_size_factors=FALSE, subset_row=subset.row, exprs_values=exprs_values)
+    nbatches <- length(batches)
     collected.ratios <- matrix(1, nbatches, nbatches)
 
     for (first in seq_len(nbatches-1L)) {
@@ -38,21 +82,22 @@ multiBatchNorm <- function(..., assay.type="counts", norm.args=list(), min.mean=
         }
     }
 
-    # Finding the smallest ratio, and using that as the reference.
+    # Rescaling to the lowest coverage.
     smallest <- which.min(apply(collected.ratios, 2, min, na.rm=TRUE))
     
     for (idx in seq_along(batches)) {
         current <- batches[[idx]]
-        cursf <- sizeFactors(current)
+        cursf <- sizeFactors(current, sf.type)
         if (is.null(cursf)) {
-            cursf <- librarySizeFactors(current)
-            warning("using library sizes as size factors")
+            cursf <- librarySizeFactors(current, exprs_values=exprs_values, subset_row=subset.row)
+            warning(sprintf("no %ssize factors in batch %i, using sum of counts instead",
+                if (is.null(sf.type)) "" else paste(sf.type, ""), idx))
         }
     
         # Adjusting the size factors for the new reference.
         cursf <- cursf/mean(cursf) / collected.ratios[idx,smallest]
-        sizeFactors(current) <- cursf
-        batches[[idx]] <- do.call(normalize, c(list(current, exprs_values=assay.type, centre_size_factors=FALSE), norm.args))
+        sizeFactors(current, sf.type) <- cursf
+        batches[[idx]] <- current
     }
 
     return(batches)
