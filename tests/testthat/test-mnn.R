a9b0324cd71c38f36d94c4642797697aa34bb263
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-05 14:42:08 +0100

    Renamed cosine normalization function in tests.

--- tests/testthat/test-mnn.R
+++ tests/testthat/test-mnn.R
@@ -6,12 +6,12 @@ test_that("Cosine normalization is correct", {
     X <- matrix(rnorm(10000), ncol=100)
     cellnorm <- pmax(1e-8, sqrt(colSums(X^2)))
     ref <- X/matrix(cellnorm, nrow(X), ncol(X), byrow=TRUE)
-    expect_equal(ref, scran:::cosine.norm(X))
+    expect_equal(ref, scran:::cosineNorm(X))
 
     X <- matrix(rpois(20000, lambda=5), ncol=1000)
     cellnorm <- pmax(1e-8, sqrt(colSums(X^2)))
     ref <- X/matrix(cellnorm, nrow(X), ncol(X), byrow=TRUE)
-    expect_equal(ref, scran:::cosine.norm(X))
+    expect_equal(ref, scran:::cosineNorm(X))
 })
 
 set.seed(10000)
