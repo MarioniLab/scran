6b95aca006bf41b555a8765cb885b1766074b950
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-24 17:36:28 +0100

    Added tests for doubletCells.

--- tests/testthat/test-doublet.R
+++ tests/testthat/test-doublet.R
@@ -1,4 +1,4 @@
-# This tests the doublet-discovery machinery, 
+# This tests the doublet-discovery machinery,
 # though there is little to do other than run the function.
 # library(scran); library(testthat); source("test-doublet.R")
 
@@ -14,19 +14,21 @@ counts.m <- matrix(rpois(ngenes*20, mu1+mu2), nrow=ngenes)
 counts <- cbind(counts.1, counts.2, counts.m)
 clusters <- rep(1:3, c(ncol(counts.1), ncol(counts.2), ncol(counts.m)))
 
+###################################################
+
 test_that("doubletCluster works correctly with vanilla tests", {
-	dbl <- doubletCluster(counts, clusters)
+    dbl <- doubletCluster(counts, clusters)
     expect_identical(rownames(dbl)[1], "3")
     expect_identical(dbl$source1[1], "2")
     expect_identical(dbl$source2[1], "1")
-	
-	# Checking the relative library sizes.
+
+    # Checking the relative library sizes.
     ls1 <- median(colSums(counts.1))
     ls2 <- median(colSums(counts.2))
     ls3 <- median(colSums(counts.m))
 
-	expect_equal(dbl$lib.size1[1], ls2/ls3)
-	expect_equal(dbl$lib.size2[1], ls1/ls3)
+    expect_equal(dbl$lib.size1[1], ls2/ls3)
+    expect_equal(dbl$lib.size2[1], ls1/ls3)
 
     # Checking the proportions.
     expect_equal(dbl$prop,
@@ -66,30 +68,145 @@ test_that("doubletCluster works correctly with row subsets", {
 
 test_that("doubletCluster works correctly with SingleCellExperiment", {
     sce <- SingleCellExperiment(list(counts=counts))
-	ref <- doubletCluster(counts, clusters)
-	dbl <- doubletCluster(sce, clusters)
+    ref <- doubletCluster(counts, clusters)
+    dbl <- doubletCluster(sce, clusters)
     expect_identical(ref, dbl)
 
     # With a different assay.
     assay(sce, "whee") <- counts + rpois(length(counts), lambda=2)
-	ref2 <- doubletCluster(assay(sce, "whee"), clusters)
-	dbl2 <- doubletCluster(sce, clusters, assay.type="whee")
+    ref2 <- doubletCluster(assay(sce, "whee"), clusters)
+    dbl2 <- doubletCluster(sce, clusters, assay.type="whee")
     expect_identical(ref2, dbl2)
 
     # With spike-ins that get used.
     isSpike(sce, "ERCC") <- sample(nrow(sce), 20)
-	dbl3 <- doubletCluster(sce, clusters, get.spikes=TRUE)
+    dbl3 <- doubletCluster(sce, clusters, get.spikes=TRUE)
     expect_identical(ref, dbl3)
 
     # ... or ignored.
-	dbl4 <- doubletCluster(sce, clusters)
+    dbl4 <- doubletCluster(sce, clusters)
     ref3 <- doubletCluster(counts(sce), clusters, subset.row=!isSpike(sce))
     expect_identical(ref3, dbl4)
 
     # With both spike-ins _and_ subset.row specified.
     keep <- c(sample(which(isSpike(sce)), 10), sample(which(!isSpike(sce)), 10))
-   	dbl5 <- doubletCluster(sce, clusters, subset.row=keep)
+       dbl5 <- doubletCluster(sce, clusters, subset.row=keep)
     ref4 <- doubletCluster(counts(sce), clusters, subset.row=setdiff(keep, which(isSpike(sce))))
     expect_identical(ref4, dbl5)
+})
+
+###################################################
+
+set.seed(9900002)
+test_that("doubletCells PC spawning works correctly", {
+    sf <- runif(ncol(counts))
+    y <- log2(t(t(counts)/sf)+1)
+    centers <- rowMeans(y)
+    SVD <- svd(t(y - centers), nv=20)
+
+    set.seed(12345)
+    sim.pcs <- scran:::.spawn_doublet_pcs(counts, sf, SVD$v, centers, niters=10000L, block=10000L)
+
+    set.seed(12345)
+    L <- sample(ncol(counts), 10000L, replace=TRUE)
+    R <- sample(ncol(counts), 10000L, replace=TRUE)
+    ref.x <- counts[,L] + counts[,R]
+    ref.y <- log2(t(t(ref.x)/(sf[L] + sf[R]))+1)
+    ref.pcs <- crossprod(ref.y - centers, SVD$v)
+
+    expect_equal(sim.pcs, ref.pcs)
+
+    # Works with multiple iterations.
+    set.seed(23456)
+    sim.pcs <- scran:::.spawn_doublet_pcs(counts, sf, SVD$v, centers, niters=25000L, block=10000L)
+
+    set.seed(23456)
+    ref1 <- scran:::.spawn_doublet_pcs(counts, sf, SVD$v, centers, niters=10000L, block=10000L)
+    ref2 <- scran:::.spawn_doublet_pcs(counts, sf, SVD$v, centers, niters=10000L, block=10000L)
+    ref3 <- scran:::.spawn_doublet_pcs(counts, sf, SVD$v, centers, niters=5000L, block=10000L)
+
+    expect_equal(sim.pcs, rbind(ref1, ref2, ref3))
+    expect_identical(dim(sim.pcs), c(25000L, ncol(SVD$v)))
+})
+
+set.seed(9900003)
+test_that("size factor variations in doubletCells work correctly", {
+    sf1 <- runif(ncol(counts))
+    sf2 <- runif(ncol(counts))
+
+    set.seed(12345)
+    out <- doubletCells(counts)
+    set.seed(12345)
+    ref <- doubletCells(counts, size.factors.norm=scater::librarySizeFactors(counts))
+    expect_equal(out, ref)
+
+    set.seed(23456)
+    out <- doubletCells(counts, size.factors.norm=sf1)
+    set.seed(23456)
+    ref <- doubletCells(t(t(counts)/sf1), size.factors.norm=rep(1, ncol(counts)), size.factors.content=1/sf1)
+    expect_equal(out, ref)
+
+    set.seed(34567)
+    out <- doubletCells(counts, size.factors.norm=sf1, size.factors.content=sf2)
+    set.seed(34567)
+    ref <- doubletCells(t(t(counts)/sf2), size.factors.norm=sf1/sf2)
+    expect_equal(out, ref)
+})
+
+set.seed(9900004)
+test_that("high-level tests for doubletCells work correctly", {
+    mu1 <- 2^rnorm(ngenes)
+    mu2 <- 2^rnorm(ngenes)
+    ncA <- 100
+    ncB <- 100
+    ncC <- 20
+
+    counts.A <- matrix(mu1, ncol=ncA, nrow=ngenes)
+    counts.B <- matrix(mu2, ncol=ncB, nrow=ngenes)
+    counts.C <- matrix(mu1+mu2, ncol=ncC, nrow=ngenes)
+    clusters <- rep(1:3, c(ncA, ncB, ncC))
+
+    out <- doubletCells(jitter(cbind(counts.A, counts.B, counts.C)))
+    expect_true(min(out[clusters==3]) > max(out[clusters!=3]))
+
+    # Now with differences in RNA content.
+    counts.A <- matrix(mu1, ncol=ncA, nrow=ngenes)
+    counts.B <- matrix(mu2, ncol=ncB, nrow=ngenes)
+    counts.C <- matrix(mu1+2*mu2, ncol=ncC, nrow=ngenes)
+    sf.spike <- 1/rep(1:3, c(ncA, ncB, ncC))
+
+    out <- doubletCells(jitter(cbind(counts.A, counts.B, counts.C)), size.factors.content=sf.spike)
+    expect_true(min(out[clusters==3]) > max(out[clusters!=3]))
+
+    out <- doubletCells(jitter(cbind(counts.A, counts.B, counts.C))) # fails without size factor info.
+    expect_true(max(out[clusters==3]) < min(out[clusters!=3]))
+})
+
+set.seed(9900005)
+test_that("other settings for doubletCells work correctly", {
+    # Subsetting behaves correctly.
+    set.seed(1000)
+    sim <- doubletCells(counts, subset.row=1:50)
+    set.seed(1000)
+    ref <- doubletCells(counts[1:50,])
+    expect_identical(sim, ref)
+
+    # Warnings raised if too many neighbors are requested.
+    expect_warning(doubletCells(counts, k=1000), "'k' capped")
+
+    # IRLBA works correctly.
+    set.seed(2000)
+    sim <- doubletCells(counts, d=5)
+    set.seed(2000)
+    ref <- doubletCells(counts, approximate=TRUE, irlba.args=list(tol=1e-12), d=5)
+    expect_true(all( abs(sim-ref)/(sim+ref+1e-6) < 0.1 ) )
+
+    # Responds correctly to blocking.
+    set.seed(3000)
+    ref <- doubletCells(counts)
+    sim1 <- doubletCells(counts, block=1000)
+    expect_equal(sim1, ref, tol=0.1)
+    sim2 <- doubletCells(counts, niters=20000)
+    expect_equal(sim2, ref, tol=0.1)
 })
 
