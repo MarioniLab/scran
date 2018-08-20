4b992ec3fedd506b211b416173596d2ff4ab4e5a
LTLA <alun@wehi.edu.au>
2018-08-20 14:34:19 +0100

    Parallelized makeTechTrend.

--- R/makeTechTrend.R
+++ R/makeTechTrend.R
@@ -2,8 +2,9 @@
 #' @importFrom Matrix colSums rowMeans
 #' @importFrom SingleCellExperiment logcounts
 #' @importFrom stats splinefun
+#' @importFrom BiocParallel SerialParam bplapply
 #' @export
-makeTechTrend <- function(means, size.factors=1, tol=1e-6, dispersion=0, pseudo.count=1, x=NULL) 
+makeTechTrend <- function(means, size.factors=1, tol=1e-6, dispersion=0, pseudo.count=1, x=NULL, BPPARAM=SerialParam()) 
 # This generates NB-distributed counts with the specified 
 # dispersion in order to fit the mean-variance trend to the
 # log-normalized counts. Designed for droplet data where
@@ -34,11 +35,20 @@ makeTechTrend <- function(means, size.factors=1, tol=1e-6, dispersion=0, pseudo.
     }
 
     # Calling the C++ code to do the heavy lifting.
-    collected <- .Call(cxx_calc_log_count_stats, means, size.factors, tol, dispersion, pseudo.count)
-    collected.means <- collected[[1]]
-    collected.vars <- collected[[2]]
+    to.core <- .worker_assign(length(means), BPPARAM)
+    by.core <- .split_vector_by_workers(means, to.core)
+    raw.out <- bplapply(by.core, FUN=.tech_trend_computer, size.factors=size.factors, 
+        tol=tol, dispersion=dispersion, pseudo.count=pseudo.count, BPPARAM=BPPARAM)
+
+    collected.means <- unlist(lapply(raw.out, "[[", i=1))
+    collected.vars<- unlist(lapply(raw.out, "[[", i=2))
 
     # Creating a spline output function.
     splinefun(collected.means, collected.vars)
 }
 
+.tech_trend_computer <- function(means, size.factors, tol, dispersion, pseudo.count) 
+# A helper function to ensure that the scran namespace is used in SnowParam().
+{
+    .Call(cxx_calc_log_count_stats, means, size.factors, tol, dispersion, pseudo.count)
+}
