1d3d52485d1640e50f8ae51d21b94bb8461bf305
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-03 16:39:47 +0100

    Removed subset_and_divide, as pre-scaling is no longer performed. Modified forge_system to directly accept integer matrices.

--- src/forge_system.cpp
+++ src/forge_system.cpp
@@ -1,92 +1,9 @@
 #include "scran.h"
 
-/* A function to (a) subset by row, (b) subset by column, and (c) divide through by the library sizes. 
- * The output is equivalent to t(t(MAT[row_subset,col_subset])/lib.sizes) where lib.sizes itself is
- * computed as colSums(MAT[row_subset,col_subset]).
- */
-
-template <class V, class M>
-SEXP subset_and_divide_internal(M in, SEXP row_subset, SEXP col_subset) {
-    // Checking subset vectors
-    auto rsubout=check_subset_vector(row_subset, in->get_nrow());
-    const size_t rslen=rsubout.size();
-    auto csubout=check_subset_vector(col_subset, in->get_ncol());
-    const size_t cslen=csubout.size();
-
-    V incoming(in->get_nrow());
-    size_t start_row=0, end_row=0;
-    if (rslen) {
-        // Cutting out extraction costs for unneeded start/end elements.
-        start_row=*std::min_element(rsubout.begin(), rsubout.end());
-        end_row=*std::max_element(rsubout.begin(), rsubout.end())+1;
-    }
-
-    // Setting up the output structures - only ever using in-memory matrices, as computeSumFactors() can't handle too many cells anyway.
-    Rcpp::NumericVector libsizes(cslen);
-    Rcpp::NumericVector outgoing(rslen), averaged(rslen);
-
-    beachmat::output_param oparam=(in->get_matrix_type()==beachmat::SPARSE ? beachmat::SPARSE_PARAM : beachmat::SIMPLE_PARAM);
-    auto omat=beachmat::create_numeric_output(rslen, cslen, oparam);
-
-    auto lbIt=libsizes.begin();
-    size_t cs=0;
-    for (const auto& c : csubout) {
-
-        // Extracting the column, subsetting the rows.
-        auto inIt=in->get_const_col(c, incoming.begin(), start_row, end_row);
-        auto oIt=outgoing.begin();
-        for (const auto& r : rsubout) {
-            (*oIt)=*(inIt + r - start_row);
-            ++oIt;
-        }
-           
-        // Dividing by the library size. 
-        const double& curlib=((*lbIt)=std::accumulate(outgoing.begin(), outgoing.end(), 0.0));
-        if (curlib < 0.00000001) {
-            throw std::runtime_error("cells should have non-zero library sizes");
-        }
-        ++lbIt;
-
-        for (double& out : outgoing) {
-            out/=curlib;
-        }
-        omat->set_col(cs, outgoing.begin());
-        ++cs;
-
-        // Adding to the average.
-        oIt=outgoing.begin();
-        for (auto& a : averaged) {
-            a+=(*oIt);
-            ++oIt;
-        }
-    }
-
-    // Dividing the average.
-    for (auto& a : averaged) {
-        a/=cslen;
-    }
-
-    return Rcpp::List::create(libsizes, omat->yield(), averaged); 
-}
-
-SEXP subset_and_divide(SEXP matrix, SEXP row_subset, SEXP col_subset) {
-    BEGIN_RCPP
-    int rtype=beachmat::find_sexp_type(matrix);
-    if (rtype==INTSXP) {
-        auto input=beachmat::create_integer_matrix(matrix);
-        return subset_and_divide_internal<Rcpp::IntegerVector>(input.get(), row_subset, col_subset);
-    } else {
-        auto input=beachmat::create_numeric_matrix(matrix);
-        return subset_and_divide_internal<Rcpp::NumericVector>(input.get(), row_subset, col_subset);
-    }
-    END_RCPP
-}
-
 /*** A function to estimate the pooled size factors and construct the linear equations. ***/
 
-SEXP forge_system (SEXP exprs, SEXP ref, SEXP ordering, SEXP poolsizes) {
-    BEGIN_RCPP
-    auto emat=beachmat::create_numeric_matrix(exprs);
+template <class V, class M>
+SEXP forge_system_internal (M emat, SEXP ref, SEXP ordering, SEXP poolsizes) {
     const size_t ngenes=emat->get_nrow();
     const size_t ncells=emat->get_ncol();
     if (ncells==0) { throw std::runtime_error("at least one cell required for normalization"); }
@@ -119,8 +36,8 @@ SEXP forge_system (SEXP exprs, SEXP ref, SEXP ordering, SEXP poolsizes) {
     }
 
     // Filling up the cell vector.
-    Rcpp::NumericVector all_collected(last_size*ngenes);
-    std::deque<Rcpp::NumericVector::const_iterator> collected;
+    V all_collected(last_size*ngenes);
+    std::deque<typename V::const_iterator> collected;
     auto acIt=all_collected.begin();
     collected.push_back(acIt); // unfilled first vector, which gets dropped and refilled in the first iteration anyway.
     acIt+=ngenes;
@@ -179,7 +96,7 @@ SEXP forge_system (SEXP exprs, SEXP ref, SEXP ordering, SEXP poolsizes) {
                 (*rIt)=(*cIt)/(*pcIt);
             }
 
-            // Computing the median (faster than partial sort).
+            // Computing the median.
             std::nth_element(ratios.begin(), ratios.begin()+halfway, ratios.end());
             if (is_even) {
                 double medtmp=ratios[halfway];
@@ -189,19 +106,21 @@ SEXP forge_system (SEXP exprs, SEXP ref, SEXP ordering, SEXP poolsizes) {
                 pool_factor[rownum]=ratios[halfway];
             }       
         }
-
-/*
-        std::partial_sort(combined, combined+halfway+1, combined+ngenes);
-        if (is_even) {
-            ofptr[cell]=(combined[halfway]+combined[halfway-1])/2;
-        } else {
-            ofptr[cell]=combined[halfway];
-        } 
-*/
     }    
 
     return Rcpp::List::create(row_num, col_num, pool_factor);
-    END_RCPP
 }
 
+SEXP forge_system(SEXP exprs, SEXP ref, SEXP ordering, SEXP poolsizes) {
+    BEGIN_RCPP
+    int rtype=beachmat::find_sexp_type(exprs);
+    if (rtype==INTSXP) {
+        auto input=beachmat::create_integer_matrix(exprs);
+        return forge_system_internal<Rcpp::IntegerVector>(input.get(), ref, ordering, poolsizes);
+    } else {
+        auto input=beachmat::create_numeric_matrix(exprs);
+        return forge_system_internal<Rcpp::NumericVector>(input.get(), ref, ordering, poolsizes);
+    }
+    END_RCPP
+}
 
