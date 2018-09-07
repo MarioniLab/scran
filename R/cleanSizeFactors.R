f111874e62bfe4111161141f98608a7389987feb
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-07 19:37:05 +0100

    Improved initial estimates for cleanSizeFactors fit.

--- R/cleanSizeFactors.R
+++ R/cleanSizeFactors.R
@@ -1,5 +1,5 @@
 #' @export
-#' @importFrom stats nls residuals median coef
+#' @importFrom stats nls residuals median coef lm
 cleanSizeFactors <- function(size.factors, num.detected, control=nls.control(warnOnly=TRUE), iterations=3, nmads=3, ...) {
     keep <- size.factors > 0
     if (all(keep)) {
@@ -15,15 +15,25 @@ cleanSizeFactors <- function(size.factors, num.detected, control=nls.control(war
         stop("need at least three positive values for trend fitting")
     }
 
-    # Robustifying iterations with tricube weights.
-    init <- c(A=1, B=1/max(Y))
-    init <- log(init) # using log to guarantee positivity.
-    weights <- rep(1, length(Y))
-    lY <- log(Y)
+    # Solving for initial conditions.
+    lower <- median(size.factors) > size.factors
+    fit <- lm(num.detected[lower] ~ 0 + size.factors[lower])
+    A <- unname(coef(fit))
 
+    below <- which(Y < A*X)
+    top <- below[which.max(Y[below])]
+    B <- A / Y[top] - 1 / X[top] # must be positive due to 'below'.
+
+    # Using logs to enforce positivity.
+    init <- log(c(logA=A, logB=B)) 
+    lY <- log(Y)
+    lX <- log(X)
+
+    # Robustifying iterations with tricube weights.
+    weights <- rep(1, length(Y))
     for (i in seq_len(iterations+1L)) {
-        # Log-transforming both sides ensure positivity, avoid large libraries dominating the least-squares.
-        fit <- nls(lY ~ A + log(X) - log(X * exp(B) + 1), start=init, weights=weights, control=control, ...)
+        # Log-transforming both sides avoids large libraries dominating the least-squares.
+        fit <- nls(lY ~ logA + lX - log(X * exp(logB) + 1), start=init, weights=weights, control=control, ...)
         init <- coef(fit)
 
         resids <- abs(residuals(fit))
@@ -33,10 +43,10 @@ cleanSizeFactors <- function(size.factors, num.detected, control=nls.control(war
 
     # Fitting negative size factors to this trend.
     failed <- num.detected[!keep]
-    coefs <- exp(coef(fit))
-    new.sf <- failed/(coefs["A"] - coefs["B"] * failed)
+    coefs <- coef(fit)
+    new.sf <- failed/(exp(coefs["logA"]) - exp(coefs["logB"]) * failed)
 
-    # If failed > A/B (very unlikely), we set it to the maximum SF. 
+    # If any failed > A/B (very unlikely), we set it to the maximum positive SF. 
     new.sf[new.sf < 0] <- max(X)
 
     size.factors[!keep] <- new.sf
