535cbd0792381db3f2baa3f76058ee2970fc3650
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-16 12:56:13 +0100

    Rolled out specific function for p-value combining.

---
+++ R/combinePValues.R
@@ -0,0 +1,64 @@
+#' @export
+#' @importFrom stats pnorm qnorm pchisq
+combinePValues <- function(..., method=c("fisher", "z", "simes", "berger"), weights=NULL, log.p=FALSE) 
+# Combines p-values with a variety of methods.
+# Weights are support in Stouffer's Z method.
+#
+# written by Aaron Lun
+# created 16 September 2018
+{
+    input <- list(...)
+    Np <- unique(lengths(input))
+    if (length(Np) != 1) {
+        stop("all p-value vectors must have the same length")
+    }
+
+    method <- match.arg(method)
+    switch(method,
+        fisher={
+            if (log.p) {
+                all.logp <- input
+            } else {
+                all.logp <- lapply(input, FUN=log)
+            }
+
+            n <- integer(Np)
+            X <- numeric(Np)
+            for (i in seq_along(all.logp)) {
+                current <- all.logp[[i]]
+                keep <- !is.na(current)
+                X[keep] <- X[keep] + current[keep]
+                n <- n + keep
+            }
+            
+            n[n==0] <- NA_real_ # ensure that we get NA outputs.
+            pchisq(-2*X, df=2*n, lower.tail=FALSE)
+        },
+        simes={
+            .Call(cxx_combine_simes, input, log.p)
+        }, 
+        z={
+            if (is.null(weights)) {
+                weights <- rep(1, length(input))
+            } else if (length(weights)!=length(input)) {
+                stop("'length(weights)' must be equal to number of vectors in '...'")
+            } else if (any(is.na(weights) | weights<=0)) {
+                stop("weights must be positive")
+            }
+
+            Z <- W2 <- numeric(Np)
+            for (i in seq_along(input)) {
+                current <- input[[i]]
+                keep <- !is.na(current)
+                Z[keep] <- Z[keep] + qnorm(current[keep], log.p=log.p) * weights[i]
+                W2 <- W2 + ifelse(keep, weights[i]^2, 0)
+            }
+
+            W2[W2==0] <- NA_real_ # ensure we get NA outputs.
+            pnorm(Z/sqrt(W2), log.p=log.p) 
+        },
+        berger={
+            do.call(pmax, c(input, list(na.rm=TRUE)))
+        }
+    )
+}
