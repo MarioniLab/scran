05c97fc28dd3e40648d1fea1495cf5a097917fff
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-16 13:06:53 +0100

    Updated Simes C++ code to accept a list.

--- src/combine_simes.cpp
+++ src/combine_simes.cpp
@@ -1,27 +1,38 @@
 #include "scran.h"
 #include "utils.h"
 
-SEXP combine_simes(SEXP _pvals, SEXP dolog) {
+SEXP combine_simes(SEXP pvals, SEXP dolog) {
     BEGIN_RCPP 
-    Rcpp::NumericMatrix pvals(_pvals);
-    const size_t ncon=pvals.ncol();
-    const size_t ngenes=pvals.nrow();
+    Rcpp::List Pvals(pvals);
+    const size_t ncon=Pvals.size();
 
-    // Should we process these values as if they were log-transformed?
+    std::vector<Rcpp::NumericVector> individual(ncon);
+    size_t ngenes=0;
+    for (size_t c=0; c<ncon; ++c) {
+        auto& current=(individual[c]=Pvals[c]);
+        if (c==0) {
+            ngenes=current.size();
+        } else if (ngenes!=current.size()) {
+            throw std::runtime_error("p-value vectors must be of the same length");           
+        }
+    }
+    
     const bool logp=check_logical_scalar(dolog, "log-transformed specifier");
 
+    // Should we process these values as if they were log-transformed?
     Rcpp::NumericVector output(ngenes, (logp ? 0 : 1));
     std::vector<double> collected(ncon);
 
     for (size_t g=0; g<ngenes; ++g) {
-        const auto currow=pvals.row(g);
-        std::copy(currow.begin(), currow.end(), collected.begin());
+        for (size_t c=0; c<ncon; ++c) {
+            collected[c]=individual[c][g];
+        }
         std::sort(collected.begin(), collected.end());
 
         int counter=0;
         double& minval=output[g];
         for (auto P : collected) {
-            if (isNA(P)) {
+            if (ISNA(P)) {
                 continue;
             }
             ++counter;
