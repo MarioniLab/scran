ce9eb8a20d29835d155d904f82a9892d628c458e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-12 10:42:27 +0000

    Expanded quickCluster tests for new behaviour and options.

--- tests/testthat/test-quickclust.R
+++ tests/testthat/test-quickclust.R
@@ -8,61 +8,78 @@ ngenes <- 1000
 dummy <- matrix(rnbinom(ncells*ngenes, mu=10, size=20), ncol=ncells, nrow=ngenes)
 known.clusters <- sample(3, ncells, replace=TRUE)
 dummy[1:300,known.clusters==1L] <- 0
-dummy[301:600,known.clusters==2L] <- 0  
+dummy[301:600,known.clusters==2L] <- 0
 dummy[601:900,known.clusters==3L] <- 0
-    
+
 test_that("quickCluster works in the simple case", {
     emp.clusters <- quickCluster(dummy)
     expect_true(length(unique(paste0(known.clusters, emp.clusters)))==3L)
+    emp.clusters <- quickCluster(dummy, d=20)
+    expect_true(length(unique(paste0(known.clusters, emp.clusters)))==3L)
+
+    # Behaves after rows are shuffled.
     shuffled <- c(1:50, 301:350, 601:650)
-    expect_identical(quickCluster(dummy, subset.row=shuffled), emp.clusters)
+    shuf.clusters <- quickCluster(dummy, subset.row=shuffled)
+    expect_true(length(unique(paste0(shuf.clusters, emp.clusters)))==3L)
+
+    # Behaves with use.ranks=TRUE.
+    emp.clusters <- quickCluster(dummy, use.ranks=TRUE)
+    expect_true(length(unique(paste0(known.clusters, emp.clusters)))==3L)
+    emp.clusters <- quickCluster(dummy, use.ranks=TRUE, d=20)
+    expect_true(length(unique(paste0(known.clusters, emp.clusters)))==3L)
+
+    # Behaves when PCA is turned off.
+    emp.clusters <- quickCluster(dummy, d=NA)
+    expect_true(length(unique(paste0(known.clusters, emp.clusters)))==3L)
+    emp.clusters <- quickCluster(dummy, d=NA, use.ranks=TRUE)
+    expect_true(length(unique(paste0(known.clusters, emp.clusters)))==3L)
 })
 
 set.seed(300001)
-test_that("quickCluster is consistent with clustering on correlations", {
+test_that("quickCluster with use.ranks=TRUE is consistent with clustering on correlations", {
     mat <- matrix(rpois(10000, lambda=5), nrow=20)
-    obs <- quickCluster(mat)
-    
+    obs <- quickCluster(mat, use.ranks=TRUE, method="hclust", d=NA)
+
     refM <- sqrt(0.5*(1 - cor(mat, method="spearman")))
     distM <- as.dist(refM)
     obsM <- dist(scaledColRanks(mat, transposed=TRUE))
     expect_equal(as.matrix(obsM), as.matrix(distM))
 
     htree <- hclust(distM, method='ward.D2')
-    clusters <- unname(dynamicTreeCut::cutreeDynamic(htree, minClusterSize=200, distM=refM, verbose=0))
+    clusters <- unname(dynamicTreeCut::cutreeDynamic(htree, minClusterSize=100, distM=refM, verbose=0))
     expect_identical(clusters, as.integer(obs)) # this can complain if unassigned, as 0 becomes 1 in as.integer().
-
-    # Works with min.size set.
-    obs <- quickCluster(mat, min.size=50)
-    clusters <- unname(dynamicTreeCut::cutreeDynamic(htree, minClusterSize=50, distM=refM, verbose=0))
-    expect_identical(clusters, as.integer(obs))
 })
 
+set.seed(3000011)
 test_that("quickCluster functions correctly with subsetting", {
+    mat <- matrix(rpois(20000, lambda=1:100), nrow=100)
+
     # Works properly with subsetting.
-    mat <- matrix(rpois(10000, lambda=5), nrow=20)
-    subset.row <- 15:1 
+    subset.row <- sample(nrow(mat), nrow(mat)/2)
     obs <- quickCluster(mat, min.size=50, subset.row=subset.row)
     expect_identical(obs, quickCluster(mat[subset.row,], min.size=50)) # Checking that it behaves properly.
-    expect_false(identical(quickCluster(mat), obs)) # It should return different results.
+    expect_false(identical(quickCluster(mat, min.size=50), obs)) # It should return different results.
 
-    refM <- sqrt(0.5*(1 - cor(mat[subset.row,], method="spearman")))
-    distM <- as.dist(refM) 
-    htree <- hclust(distM, method='ward.D2')
-    clusters <- unname(dynamicTreeCut::cutreeDynamic(htree, minClusterSize=50, distM=refM, verbose=0))
-    expect_identical(clusters, as.integer(obs))
+    # Same for ranks (use hclust to avoid issues with tied neighbors in rank space).
+    obs <- quickCluster(mat, min.size=50, subset.row=subset.row, use.ranks=TRUE, method="hclust")
+    expect_identical(obs, quickCluster(mat[subset.row,], min.size=50, use.ranks=TRUE, method="hclust"))
+    expect_false(identical(quickCluster(mat, min.size=50, use.ranks=TRUE, method="hclust"), obs))
 
-    # Handles the mean.
+    # Handles the mean, but only when use.ranks=TRUE.
+    obs <- quickCluster(mat, min.size=50, min.mean=5, use.ranks=TRUE)
+    expect_identical(obs, quickCluster(mat, min.size=50, use.ranks=TRUE, subset.row=scater::calcAverage(mat) >= 5))
+
     obs <- quickCluster(mat, min.size=50, min.mean=5)
-    expect_identical(obs, quickCluster(mat, min.size=50, subset.row=scater::calcAverage(mat) >= 5))
+    expect_identical(obs, quickCluster(mat, min.size=50, min.mean=1)) # should not respond.
 })
 
+set.seed(3000012)
 test_that("quickCluster functions correctly with blocking", {
-    # Comparison to a slow manual method        
+    # Comparison to a slow manual method
     mat <- matrix(rpois(10000, lambda=5), nrow=20)
     block <- sample(3, ncol(mat), replace=TRUE)
     obs <- quickCluster(mat, min.size=10, block=block)
-     
+
     collected <- numeric(ncol(mat))
     last <- 0L
     for (x in sort(unique(block))) {
@@ -79,28 +96,92 @@ test_that("quickCluster functions correctly with blocking", {
     expect_identical(ref, obs)
 
     # Should avoid problems with multiple BPPARAM specifications.
-    set.seed(100)
     ref <- quickCluster(mat, min.size=10, block=block, method="igraph")
-    set.seed(100)
-    obs <- quickCluster(mat, min.size=10, block=block, method="igraph", BPPARAM=SerialParam())
+    obs <- quickCluster(mat, min.size=10, block=block, method="igraph", block.BPPARAM=SerialParam())
     expect_identical(obs, ref)
 })
 
-# Checking that we're executing the igraph methods correctly.
-#
-# NOTE 1: these tests are surprising fragile, as findKNN in rank space is liable to find lots of tied distances.
-# This results in arbitrary choices and ordering of neighbors, which can differ between seeds and machines (depending on precision).
-# Hence we need to make sure that there are no ties, by supplying enough dimensions with no tied ranks.
-#
-# NOTE 2: As a result of the above note, we also need to turn off approximate PCs, to avoid issues with irlba variability.
-# Otherwise we would have to set the seed everytime, and this would mask any issues with NN detection.
+set.seed(3000013)
+test_that("quickCluster's calls to min.size in dynamic tree cut are respected", {
+    mat <- matrix(rpois(10000, lambda=5), nrow=20)
+    obs <- scran:::.quick_cluster(mat, min.size=50, method="hclust", d=NA)
+
+    ref <- scater::normalizeCounts(mat, scater::librarySizeFactors(mat), return_log=TRUE)
+    refM <- dist(t(ref))
+    htree <- hclust(refM, method="ward.D2")
+    clusters <- unname(dynamicTreeCut::cutreeDynamic(htree, minClusterSize=50, distM=as.matrix(refM), verbose=0))
+    expect_identical(clusters, as.integer(obs))
+
+    # Forcing min.size to be larger than a population size, checking for a warning upon unassigned cells.
+    ncells <- 200
+    dummy <- matrix(rpois(ncells*200, lambda=5), nrow=200)
+    known.clusters <- sample(3, ncells, replace=TRUE)
+    dummy[1:40,known.clusters==1L] <- 0
+    dummy[41:80,known.clusters==2L] <- 0
+    dummy[81:120,known.clusters==3L] <- 0
+
+    out <- quickCluster(dummy, min.size=0, method="hclust")
+    expect_identical(length(unique(paste(out, known.clusters))), 3L)
+
+    leftovers <- min(80, sum(known.clusters==3))
+    keep <- c(which(known.clusters==1), which(known.clusters==2), which(known.clusters==3)[seq_len(leftovers)]) # force cluster 3 to be unassigned.
+    expect_warning(forced <- quickCluster(dummy[,keep], method="hclust"), sprintf("%i cells were not assigned to any cluster", leftovers))
+    expect_identical(as.character(tail(forced, leftovers)), rep("0", leftovers))
+})
 
 set.seed(300002)
-test_that("quickCluster with igraph works with various settings", {
-    ncells <- 500
-    ndims <- 400
+test_that("quickCluster with igraph works correctly", {
     k <- 10
-    mat <- matrix(rnorm(ncells*ndims, mean=20), nrow=ndims)
+    mat <- matrix(rnorm(200000, mean=20), nrow=400)
+    obs <- quickCluster(mat, min.size=0, method="igraph", k=k, d=50)
+
+    ref <- scater::normalizeCounts(mat, scater::librarySizeFactors(mat), return_log=TRUE)
+    snn <- buildSNNGraph(ref, k=k, d=50)
+    out <- igraph::cluster_walktrap(snn)
+    expect_identical(factor(out$membership), obs)
+
+    obs <- quickCluster(mat, min.size=0, method="igraph", k=k, d=50, graph.fun=igraph::cluster_fast_greedy)
+    out <- igraph::cluster_fast_greedy(snn)
+    expect_identical(factor(out$membership), obs)
+
+    # Checking that 'd' is respected, along with other arguments that are passed along.
+    obs <- quickCluster(mat, min.size=0, method="igraph", d=20, k=15)
+    snn <- buildSNNGraph(ref, d=20, k=15)
+    out <- igraph::cluster_walktrap(snn)
+    expect_identical(factor(out$membership), obs)
+
+    obs <- quickCluster(mat, min.size=0, method="igraph", d=NA, k=5)
+    snn <- buildSNNGraph(ref, d=NA, k=5)
+    out <- igraph::cluster_walktrap(snn)
+    expect_identical(factor(out$membership), obs)
+})
+
+set.seed(3000021)
+test_that("quickCluster with igraph merging works correctly", {
+    k <- 10
+    mat <- matrix(rnorm(200000, mean=20), nrow=400)
+    obs <- quickCluster(mat, min.size=0, method="igraph", k=k)
+
+    min.size <- 100
+    expect_false(all(table(obs) >= min.size))
+    obs2 <- quickCluster(mat, min.size=min.size, method="igraph", k=k)
+    expect_true(all(table(obs2) >= min.size))
+
+    combined <- paste0(obs, ".", obs2)
+    expect_identical(length(unique(combined)), length(unique(obs))) # Confirm that they are nested.
+})
+
+set.seed(3000022)
+test_that("quickCluster with igraph on ranks works correctly", {
+    # NOTE 1: these tests are surprisingly fragile for use.rank=TRUE, as findKNN in rank space is liable to find lots of tied distances.
+    # This results in arbitrary choices and ordering of neighbors, which can differ between seeds and machines (depending on precision).
+    # Hence we need to make sure that there are no ties, by supplying enough dimensions with no tied ranks.
+    #
+    # NOTE 2: As a result of the above note, we also need to turn off approximate PCs, to avoid issues with irlba variability.
+    # Otherwise we would have to set the seed everytime, and this would mask any issues with NN detection.
+    k <- 10
+    mat <- matrix(rnorm(200000, mean=20), nrow=400)
+    obs <- quickCluster(mat, min.size=0, method="igraph", k=k)
 
     # Checking that there are no ties within the 'k+1'th nearest neighbors for each cell.
     ref <- scaledColRanks(mat)
@@ -110,67 +191,28 @@ test_that("quickCluster with igraph works with various settings", {
     expect_true(min(out) > 1e-8)
 
     # Testing igraph mode.
-    obs <- quickCluster(mat, min.size=0, method="igraph", pc.approx=FALSE, k=k)
+    obs <- quickCluster(mat, min.size=0, method="igraph", k=k, use.ranks=TRUE)
     expect_identical(length(obs), ncol(mat))
 
-    snn <- buildSNNGraph(ref, pc.approx=FALSE, k=k) 
-    out <- igraph::cluster_fast_greedy(snn)
-    expect_identical(factor(out$membership), obs)
-    
-    # Checking that min.size merging works.
-    min.size <- 100 
-    expect_false(all(table(obs) >= min.size))
-    obs2 <- quickCluster(mat, min.size=min.size, method="igraph", pc.approx=FALSE, k=k)
-    expect_true(all(table(obs2) >= min.size))
-
-    combined <- paste0(obs, ".", obs2)
-    expect_identical(length(unique(combined)), length(unique(obs))) # Confirm that they are nested.
-
-    # Checking that irlba behaves here.
-    set.seed(0)
-    obs <- quickCluster(mat, min.size=0, method="igraph", pc.approx=TRUE, k=k)
-    set.seed(0)
-    snn <- buildSNNGraph(ref, pc.approx=TRUE, k=k) 
-    out <- igraph::cluster_fast_greedy(snn)
+    snn <- buildSNNGraph(ref, k=k, d=50)
+    out <- igraph::cluster_walktrap(snn)
     expect_identical(factor(out$membership), obs)
 
-    # Checking that other arguments are passed along.
-    obs <- quickCluster(mat, min.size=0, method="igraph", d=NA, pc.approx=FALSE, k=k)
-    snn <- buildSNNGraph(ref, d=NA, k=k)
-    out <- igraph::cluster_fast_greedy(snn)
+    # Passes on other parameters.
+    obs <- quickCluster(mat, min.size=0, method="igraph", k=10, d=20, use.ranks=TRUE)
+    snn <- buildSNNGraph(ref, k=10, d=20)
+    out <- igraph::cluster_walktrap(snn)
     expect_identical(factor(out$membership), obs)
 })
 
-# Creating an example where quickCluster should behave correctly.
-
-test_that("quickCluster reports the correct clusters", {
-    ncells <- 600
-    ngenes <- 200
-    count.sizes <- rnbinom(ncells, mu=100, size=5)
-    multiplier <- seq_len(ngenes)/100
-    dummy <- outer(multiplier, count.sizes)
-    
-    known.clusters <- sample(3, ncells, replace=TRUE)
-    dummy[1:40,known.clusters==1L] <- 0
-    dummy[41:80,known.clusters==2L] <- 0  
-    dummy[81:120,known.clusters==3L] <- 0
-
-    out <- quickCluster(dummy)          
-    expect_identical(length(unique(paste(out, known.clusters))), 3L)
-
-    # Checking for a warning upon unassigned cells.    
-    leftovers <- 100
-    expect_warning(forced <- quickCluster(dummy[,c(which(known.clusters==1), 
-                                                   which(known.clusters==2), 
-                                                   which(known.clusters==3)[seq_len(leftovers)])]), 
-                   sprintf("%i cells were not assigned to any cluster", leftovers))
-    expect_identical(as.character(tail(forced, leftovers)), rep("0", leftovers))
-}) 
-
 test_that("quickCluster fails on silly inputs", {
     dummy <- matrix(rpois(10000, lambda=5), nrow=20)
-    expect_error(quickCluster(dummy[0,]), "rank variances of zero detected for a cell")
-    expect_error(quickCluster(dummy[,0]), "fewer cells than the minimum cluster size")
+    expect_error(quickCluster(dummy[0,]), "need at least 2 points")
+    expect_error(quickCluster(dummy[,0]), "no residual d.f.")
+
+    expect_error(quickCluster(dummy[0,], use.ranks=TRUE), "rank variances of zero detected for a cell")
+    expect_error(quickCluster(dummy[,0], use.ranks=TRUE), "a dimension is zero")
+    expect_error(quickCluster(dummy[,0], d=NA, use.ranks=TRUE), "fewer cells than the minimum cluster size")
 })
 
 set.seed(20002)
@@ -185,10 +227,10 @@ test_that("quickCluster works on SingleCellExperiment objects", {
     isSpike(X, "ERCC") <- 1:20
     expect_identical(quickCluster(X), quickCluster(counts(X)[-(1:20),]))
     subset.row <- 1:25*2
-    expect_identical(quickCluster(X, subset.row=subset.row), 
+    expect_identical(quickCluster(X, subset.row=subset.row),
                      quickCluster(counts(X)[setdiff(subset.row, 1:20),]))
-    expect_identical(quickCluster(X, subset.row=subset.row, get.spikes=TRUE), 
-                     quickCluster(counts(X)[subset.row,]))   
+    expect_identical(quickCluster(X, subset.row=subset.row, get.spikes=TRUE),
+                     quickCluster(counts(X)[subset.row,]))
 })
 
 set.seed(20003)
