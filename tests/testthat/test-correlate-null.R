5d62dfbac3ccf0ba71519118d2d93816deaf6b41
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2018-12-29 11:46:12 +0000

    Split correlateNull and correlatePairs tests into separate files.

---
+++ tests/testthat/test-correlate-null.R
@@ -0,0 +1,131 @@
+# This checks the correlateNull function.
+# require(scran); require(testthat); source("setup.R"); source("test-correlate-null.R")
+
+refnull <- function(niters, ncells, resort=TRUE) {
+    rankings <- as.double(seq_len(ncells))
+    seeds <- scran:::.create_seeds(niters)
+    shuffled <- scramble_matrix(matrix(rankings, nrow=ncells, ncol=niters), seed=seeds)
+    out <- cor(shuffled, rankings, method="spearman")
+    if (resort) { out <- sort(out) }
+    out
+}
+
+set.seed(20000)
+test_that("null distribution of correlations is correctly calculated", {
+    set.seed(100)
+    ref <- refnull(1e3, 121)
+    set.seed(100)
+    out <- correlateNull(121, iters=1e3)
+    expect_equal(ref, as.double(out))
+    
+    set.seed(100)
+    ref <- refnull(1e3, 12)
+    set.seed(100)
+    out <- correlateNull(12, iters=1e3)
+    expect_equal(ref, as.double(out))
+})
+
+set.seed(200010) 
+test_that("C++ rnorm works correctly", {
+    vals <- .Call(scran:::cxx_test_rnorm, 20000, 1)
+    expect_identical(length(vals), 20000L)
+    expect_equal(mean(vals), 0, tol=0.01)
+    expect_equal(var(vals), 1, tol=0.01)
+    expect_identical(anyDuplicated(vals), 0L)
+
+    vals2 <- .Call(scran:::cxx_test_rnorm, 20000, 2)
+    expect_false(identical(vals, vals2))
+})
+
+set.seed(20001)
+test_that("correlateNull works with a design matrix", {
+    # Constructing a reference function.
+    REFFUN <- function(design, iters=1e3) {
+        QR <- qr(design, LAPACK=TRUE)
+        df <- nrow(design)-ncol(design)
+
+        collected <- list()
+        seeds <- scran:::.create_seeds(1e3)
+        for (x in seq_along(seeds)) {
+            vals <- .Call(scran:::cxx_test_rnorm, df*2L, seeds[x]) 
+            expect_identical(length(vals), df*2L)
+    
+            first.half <- qr.qy(QR, c(0,0, head(vals, df)))
+            second.half <- qr.qy(QR, c(0, 0, tail(vals, df)))
+            collected[[x]] <- cor(first.half, second.half, method="spearman")
+        }
+        sort(unlist(collected))
+    }
+
+    # A one-way layout.
+    design <- model.matrix(~factor(rep(c(1,2), each=10)))
+    
+    set.seed(100)
+    out1 <- REFFUN(design)
+    set.seed(100)
+    out2 <- correlateNull(design=design, iters=1e3)
+    expect_equal(out1, as.double(out2))
+    expect_equal(attr(out2, "design"), design)
+
+    # A more complicated design.
+    design <- model.matrix(~seq_len(10))
+    set.seed(200)
+    out1 <- REFFUN(design)
+    set.seed(200)
+    out2 <- correlateNull(design=design, iters=1e3)
+    expect_equal(out1, as.double(out2))
+    expect_equal(attr(out2, "design"), design)
+})
+
+test_that("correlateNull works with a blocking factor", {
+    grouping <- rep(1:5, each=3)
+    
+    set.seed(100)
+    out1 <- 0L
+    for (gl in table(grouping)) { 
+        out1 <- out1 + refnull(1e3, gl, resort=FALSE) * gl
+    }
+    out1 <- out1/length(grouping)
+    out1 <- sort(out1)
+    
+    set.seed(100)
+    out2 <- correlateNull(block=grouping, iters=1e3)
+    expect_equal(out1, as.double(out2))
+    expect_equal(attr(out2, "block"), grouping)
+    expect_identical(attr(out2, "design"), NULL)
+})
+
+test_that("correlateNull is unaffected by the number of cores", {
+    set.seed(200)
+    ref <- correlateNull(12, iters=1e3)
+
+    BPPARAM <- MulticoreParam(3) # Before set.seed, as MulticoreParam resets the seed.
+    set.seed(200)
+    out <- correlateNull(12, iters=1e3, BPPARAM=BPPARAM)
+    expect_identical(ref, out)
+
+    grouping <- sample(rep(LETTERS[1:5], each=5))
+    set.seed(300)
+    ref <- correlateNull(block=grouping, iters=1e3)
+    set.seed(300)
+    out <- correlateNull(block=grouping, iters=1e3, BPPARAM=BPPARAM)
+    expect_identical(ref, out)
+
+    design <- cbind(1, runif(30))
+    set.seed(400)
+    ref <- correlateNull(design=design, iters=1e3)
+    set.seed(400)
+    out <- correlateNull(design=design, iters=1e3, BPPARAM=BPPARAM)
+    expect_identical(ref, out)
+
+    # Does not fail with low numbers of iterations.
+    expect_error(correlateNull(12, iters=2, BPPARAM=BPPARAM), NA)
+})
+
+test_that("correlateNull works correctly on silly inputs", {
+    expect_identical(correlateNull(ncells=100, iters=0), numeric(0))
+    expect_error(correlateNull(ncells=0), "number of cells should be greater than 2")
+    expect_error(correlateNull(ncells=100, design=design), "cannot specify both 'ncells' and 'design'")
+    expect_error(correlateNull(200, block=rep(1, 20)), "cannot specify")
+    expect_error(correlateNull(200, design=cbind(rep(1, 20))), "cannot specify")
+})
--- tests/testthat/test-correlate-pairs.R
+++ tests/testthat/test-correlate-pairs.R
@@ -1,137 +1,5 @@
 # This checks the correlateNull function.
-# require(scran); require(testthat); source("setup.R"); source("test-correlate.R")
-
-refnull <- function(niters, ncells, resort=TRUE) {
-    rankings <- as.double(seq_len(ncells))
-    seeds <- scran:::.create_seeds(niters)
-    shuffled <- scramble_matrix(matrix(rankings, nrow=ncells, ncol=niters), seed=seeds)
-    out <- cor(shuffled, rankings, method="spearman")
-    if (resort) { out <- sort(out) }
-    out
-}
-
-set.seed(20000)
-test_that("null distribution of correlations is correctly calculated", {
-    set.seed(100)
-    ref <- refnull(1e3, 121)
-    set.seed(100)
-    out <- correlateNull(121, iters=1e3)
-    expect_equal(ref, as.double(out))
-    
-    set.seed(100)
-    ref <- refnull(1e3, 12)
-    set.seed(100)
-    out <- correlateNull(12, iters=1e3)
-    expect_equal(ref, as.double(out))
-})
-
-set.seed(200010) 
-test_that("C++ rnorm works correctly", {
-    vals <- .Call(scran:::cxx_test_rnorm, 20000, 1)
-    expect_identical(length(vals), 20000L)
-    expect_equal(mean(vals), 0, tol=0.01)
-    expect_equal(var(vals), 1, tol=0.01)
-    expect_identical(anyDuplicated(vals), 0L)
-
-    vals2 <- .Call(scran:::cxx_test_rnorm, 20000, 2)
-    expect_false(identical(vals, vals2))
-})
-
-set.seed(20001)
-test_that("correlateNull works with a design matrix", {
-    # Constructing a reference function.
-    REFFUN <- function(design, iters=1e3) {
-        QR <- qr(design, LAPACK=TRUE)
-        df <- nrow(design)-ncol(design)
-
-        collected <- list()
-        seeds <- scran:::.create_seeds(1e3)
-        for (x in seq_along(seeds)) {
-            vals <- .Call(scran:::cxx_test_rnorm, df*2L, seeds[x]) 
-            expect_identical(length(vals), df*2L)
-    
-            first.half <- qr.qy(QR, c(0,0, head(vals, df)))
-            second.half <- qr.qy(QR, c(0, 0, tail(vals, df)))
-            collected[[x]] <- cor(first.half, second.half, method="spearman")
-        }
-        sort(unlist(collected))
-    }
-
-    # A one-way layout.
-    design <- model.matrix(~factor(rep(c(1,2), each=10)))
-    
-    set.seed(100)
-    out1 <- REFFUN(design)
-    set.seed(100)
-    out2 <- correlateNull(design=design, iters=1e3)
-    expect_equal(out1, as.double(out2))
-    expect_equal(attr(out2, "design"), design)
-
-    # A more complicated design.
-    design <- model.matrix(~seq_len(10))
-    set.seed(200)
-    out1 <- REFFUN(design)
-    set.seed(200)
-    out2 <- correlateNull(design=design, iters=1e3)
-    expect_equal(out1, as.double(out2))
-    expect_equal(attr(out2, "design"), design)
-})
-
-test_that("correlateNull works with a blocking factor", {
-    grouping <- rep(1:5, each=3)
-    
-    set.seed(100)
-    out1 <- 0L
-    for (gl in table(grouping)) { 
-        out1 <- out1 + refnull(1e3, gl, resort=FALSE) * gl
-    }
-    out1 <- out1/length(grouping)
-    out1 <- sort(out1)
-    
-    set.seed(100)
-    out2 <- correlateNull(block=grouping, iters=1e3)
-    expect_equal(out1, as.double(out2))
-    expect_equal(attr(out2, "block"), grouping)
-    expect_identical(attr(out2, "design"), NULL)
-})
-
-test_that("correlateNull is unaffected by the number of cores", {
-    set.seed(200)
-    ref <- correlateNull(12, iters=1e3)
-
-    BPPARAM <- MulticoreParam(3) # Before set.seed, as MulticoreParam resets the seed.
-    set.seed(200)
-    out <- correlateNull(12, iters=1e3, BPPARAM=BPPARAM)
-    expect_identical(ref, out)
-
-    grouping <- sample(rep(LETTERS[1:5], each=5))
-    set.seed(300)
-    ref <- correlateNull(block=grouping, iters=1e3)
-    set.seed(300)
-    out <- correlateNull(block=grouping, iters=1e3, BPPARAM=BPPARAM)
-    expect_identical(ref, out)
-
-    design <- cbind(1, runif(30))
-    set.seed(400)
-    ref <- correlateNull(design=design, iters=1e3)
-    set.seed(400)
-    out <- correlateNull(design=design, iters=1e3, BPPARAM=BPPARAM)
-    expect_identical(ref, out)
-
-    # Does not fail with low numbers of iterations.
-    expect_error(correlateNull(12, iters=2, BPPARAM=BPPARAM), NA)
-})
-
-test_that("correlateNull works correctly on silly inputs", {
-    expect_identical(correlateNull(ncells=100, iters=0), numeric(0))
-    expect_error(correlateNull(ncells=0), "number of cells should be greater than 2")
-    expect_error(correlateNull(ncells=100, design=design), "cannot specify both 'ncells' and 'design'")
-    expect_error(correlateNull(200, block=rep(1, 20)), "cannot specify")
-    expect_error(correlateNull(200, design=cbind(rep(1, 20))), "cannot specify")
-})
-
-####################################################################################################
-# Checking what happens for the error-tolerant ranking.
+# require(scran); require(testthat); source("setup.R"); source("test-correlate-pairs.R")
 
 .tolerant_rank <- function(y, tol=1e-6) {
     if (!length(y)) { return(integer(0)) }
@@ -144,6 +12,7 @@ test_that("correlateNull works correctly on silly inputs", {
     rank(y, ties.method="random")
 }
 
+set.seed(1000)
 test_that("error tolerant ranking is working correctly", {
     whee <- runif(100, -1e-16, 1e-16)
     set.seed(100)
@@ -187,9 +56,6 @@ checkCorrelations <- function(out, exprs, null.dist) {
     return(data.frame(rho=assembled.rho, pvalue=assembled.pval, FDR=p.adjust(assembled.pval, method="BH")))
 }
 
-####################################################################################################
-# This checks the correlatePairs function.
-
 set.seed(10000)
 Ngenes <- 20
 Ncells <- 100
@@ -207,11 +73,11 @@ test_that("correlatePairs works with or without a pre-specified null distributio
     expect_equal(out$rho, ref$rho)
     expect_equal(out$p.value, ref$pvalue)
     expect_equal(out$FDR, ref$FDR)
-    
+ 
     set.seed(100)
-    out <- correlatePairs(X)
+    out <- correlatePairs(X, iter=1e5)
     set.seed(100)
-    nulls <- correlateNull(Ncells)
+    nulls <- correlateNull(Ncells, iter=1e5)
     ref <- checkCorrelations(out, X, null.dist=nulls)
     
     expect_equal(out$rho, ref$rho)
