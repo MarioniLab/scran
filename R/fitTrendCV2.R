2d311cbe9ebc81f3a8bf2edae352bb1ee8dd01d2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-04 21:29:07 -0700

    Copied improvedCV2 code into the new fitTrendCV2 function.

---
+++ R/fitTrendCV2.R
@@ -0,0 +1,162 @@
+#' Fit a trend to the CV2
+#' 
+#' Fit a mean-dependent trend to the squared coefficient of variation, 
+#' computed from count data after size factor normalization.
+#'
+#' @param x A numeric matrix of counts, or a \linkS4class{SingleCellExperiment} containing such a matrix.
+#' @param size.factors A numeric vector of size factors for each cell/column in \code{x}.
+#' @param top.prop A numeric scalar in (0, 1), specifying the top percentage of high-abundance genes to use for estimating the initial value of the plateau parameter of the fitted trend.
+#' @param subset.row See \code{?"\link{scran-gene-selection}"}.
+#' @param ... For the \code{ANY} method, further arguments to pass to \code{\link{weightedLowess}} for LOWESS fitting.
+#'
+#' For the generic and SingleCellExperiment methods, further arguments to pass to the \code{ANY} method.
+#'
+#' @return 
+#' A named list is returned, containing:
+#' \describe{
+#' \item{\code{mean}:}{A numeric vector of mean normalized expression values for all features used for trend fitting.}
+#' \item{\code{cv2}:}{A numeric vector of the of CV2 values for all features used for trend fitting.}
+#' \item{\code{trend}:}{A function that returns the fitted value of the trend at any value of the mean.}
+#' \item{\code{std.dev}:}{A numeric scalar containing the robust standard deviation of the ratio of \code{var} to the fitted value of the trend across all features used for trend fitting.}
+#' }
+#'
+#' @details
+#' This function fits a mean-dependent trend to the CV2 of normalized expression values for the selected features.
+#' The fitted trend can then be used to identify highly variable genes that have CV2 values above the trend.
+#'
+#' @author Aaron Lun
+#'
+#' @examples
+#' data(example.sce)
+#'
+#' # Fitting a trend:
+#' fit.spike <- fitTrendCV2(example.sce) # defaults to using spike-ins
+#' fit.endog <- fitTrendCV2(example.sce, use.spikes=FALSE)
+#' 
+#' # Comparing the two trend fits:
+#' plot(fit.endog$mean, fit.endog$cv2, pch=16, cex=0.5,
+#'     xlab="Mean", ylab="CV2", log="xy")
+#' points(fit.spike$mean, fit.spike$cv2, col="red", pch=16)
+#' 
+#' curve(fit.endog$trend(x), add=TRUE, col="dodgerblue", lwd=3)
+#' curve(fit.spike$trend(x), add=TRUE, col="salmon", lwd=3)
+#'
+#' @name fitTrendCV2
+#' @aliases fitTrendCV2 fitTrendCV2,ANY-method fitTrendCV2,SingleCellExperiment-method
+NULL
+
+#############################
+# Defining the basic method #
+#############################
+
+#' @importFrom S4Vectors DataFrame
+#' @importFrom stats pnorm predict median
+#' @importFrom scater librarySizeFactors
+#' @importFrom DelayedMatrixStats rowVars
+#' @importFrom Matrix rowMeans
+#' @importFrom DelayedArray sweep
+#' @importFrom stats nls median quantile coef
+.fit_trend_cv2 <- function(x, size.factors=NULL, subset.row=NULL, top.prop=0.01)
+# Fits a spline to the log-CV2 values.
+#
+# written by Aaron Lun
+# created 9 February 2017
+{
+    if (is.null(size.factors)) {
+        size.factors <- librarySizeFactors(x)
+    }
+    x <- sweep(x[subset.row,,drop=FALSE], 2, size.factors, "/")
+    vars <- rowVars(x)
+    means <- rowMeans(x)
+    cv2 <- vars/means^2
+
+    # Ignoring maxed CV2 values due to an outlier (caps at the number of cells).
+    # Ignoring means of zero, obviously.
+    to.use <- cv2 < ncol(x) - 1e-8 & means > 0
+
+    y <- cv2[to.use]
+    x <- means[to.use]
+    ly <- log(y)
+    w <- .inverse_density_weights(log(x))
+
+    # Rough estimation of initial parameters.
+    B <- median(ly + log(x), na.rm=TRUE)
+    A <- median(ly[x >= quantile(x, 1-top.prop)])
+
+    fitFUN <- function(X, Y, W) {
+        nls(Y ~ log(exp(A) + exp(B)/X), start=list(A=A, B=B), weights=W)
+    }
+
+    predFUN <- function(fit) {
+        Aest <- exp(coef(fit)["A"])
+        Best <- exp(coef(fit)["B"])
+        function(x) { Aest  + Best/x }
+    }
+
+    # Robustness iterations until convergence. 
+    # Weights are computed using residuals in the *log* space,
+    # as we are currently fitting on the log-CV2 values.
+    weights <- w
+    nmads <- 6
+    tol <- 1e-8
+    max.iter <- 50
+
+    for (i in seq_len(max.iter)) {
+        fit <- fitFUN(x, ly, weights)
+
+        r <- abs(ly - log(predFUN(fit)(x)))
+        r <- r/(median(r, na.rm=TRUE) * nmads)
+        r <- pmin(r, 1)
+
+        new.weights <- (1 - r^3)^3 * w
+        if (max(abs(new.weights - weights)) < tol) {
+            break
+        }
+        weights <- new.weights
+    }
+
+    corrected <- .correct_logged_expectation(x, y, w, predFUN(fit))
+    c(list(means=means, cv2=cv2), corrected)
+}
+
+#########################
+# Setting up S4 methods #
+#########################
+
+#' @export
+#' @rdname fitTrendCV2
+setGeneric("fitTrendCV2", function(x, ...) standardGeneric("fitTrendCV2"))
+
+#' @export
+#' @rdname fitTrendCV2
+setMethod("fitTrendCV2", "ANY", .fit_trend_cv2)
+
+#' @export
+#' @rdname fitTrendCV2
+#' @importFrom SummarizedExperiment assay
+#' @importFrom SingleCellExperiment isSpike
+setMethod("fitTrendCV2", "SingleCellExperiment", function(x, subset.row=NULL, ..., assay.type="counts", use.spikes=TRUE) 
+{
+    subset.row <- .subset_to_index(subset.row, x, byrow=TRUE)
+
+    # Can use only spikes, everything but spikes, or everything.
+    if (!is.na(use.spikes)) {
+        is.spike <- isSpike(x)
+        if (is.null(is.spike)) {
+            is.spike <- logical(nrow(x))
+        }
+        is.spike <- which(is.spike)
+        if (use.spikes) {
+            if (!length(is.spike)) {
+                warning("no spike-in transcripts present for 'use.spikes=TRUE'")
+            } else {
+                subset.row <- intersect(subset.row, is.spike)
+            }
+        } else {
+            subset.row <- setdiff(subset.row, is.spike)
+        }
+    }
+
+    .fit_trend_cv2(assay(x, i=assay.type), size.factors=sizeFactors(x), ..., subset.row=subset.row) 
+})
+
