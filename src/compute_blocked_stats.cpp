86eb60fb561d18bd114d729181c2e33bcd20ccec
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-08-03 16:19:18 -0700

    Added multi-input calculation of the residual variance.

--- src/compute_blocked_stats.cpp
+++ src/compute_blocked_stats.cpp
@@ -1,14 +1,15 @@
 #include "Rcpp.h"
 #include "beachmat/numeric_matrix.h"
 #include "beachmat/integer_matrix.h"
+
 #include "utils.h"
+#include "run_dormqr.h"
+
 #include <vector>
 
-/********************************
- * Central class to compute all variance statistics.
- * This should probably use Welford's algorithm to
- * encourage column-major access in the individual functions.
- ********************************/
+/*************************************************
+ * Central classes to compute variance statistics.
+ *************************************************/
 
 class blocked_stats_calculator {
 public:
@@ -59,9 +60,34 @@ private:
     std::vector<Rcpp::IntegerVector> groups;
 };
 
-/********************************
- * Compute statistics for transformed counts.
- ********************************/
+class residual_stats_calculator {
+public:
+    residual_stats_calculator(SEXP qr, SEXP qraux) : multQ(qr, qraux, 'T'), 
+        ncoefs(multQ.get_ncoefs()), ncells(multQ.get_nobs()) {}
+
+    void compute(Rcpp::NumericVector::iterator values, double* curmean, double* curvar) {
+        auto end=values+ncells;
+        (*curmean)=std::accumulate(values, end, 0.0)/ncells;
+
+        multQ.run(values); 
+        double& v=((*curvar)=0);
+        values+=ncoefs;
+        while (values!=end) { // only using the residual effects.
+            v += (*values) * (*values);
+            ++values;
+        }
+        v /= ncells - ncoefs;
+        return;
+    }
+private:  
+    run_dormqr multQ;
+    int ncoefs, ncells;
+};
+
+/********************************************
+ * Template to compute variance statistics for 
+ * a given transformation of the counts.
+ *********************************************/
 
 template<class M, class TRANSFORMER>
 Rcpp::List compute_blocked_stats(Rcpp::List bygroup, SEXP inmat, TRANSFORMER trans) {
@@ -88,9 +114,33 @@ Rcpp::List compute_blocked_stats(Rcpp::List bygroup, SEXP inmat, TRANSFORMER tra
     return(Rcpp::List::create(outmean, outvar));
 }
 
-/********************************
+template<class M, class TRANSFORMER>
+Rcpp::List compute_residual_stats(Rcpp::RObject qr, Rcpp::RObject qraux, SEXP inmat, TRANSFORMER trans) {
+    auto emat=beachmat::create_matrix<M>(inmat);
+    const size_t ncells=emat->get_ncol();
+    const size_t ngenes=emat->get_nrow();
+
+    residual_stats_calculator RSC(qr, qraux);
+   
+    // Setting up the output objects.
+    Rcpp::NumericMatrix outvar(1, ngenes);
+    Rcpp::NumericMatrix outmean(1, ngenes);
+    Rcpp::NumericVector incoming(ncells);
+
+    for (size_t counter=0; counter<ngenes; ++counter) {
+        emat->get_row(counter, incoming.begin());
+        trans(incoming.begin(), incoming.end());
+        auto curvarrow=outvar.column(counter);
+        auto curmeanrow=outmean.column(counter);
+        RSC.compute(incoming.begin(), curmeanrow.begin(),  curvarrow.begin());
+    }
+
+    return(Rcpp::List::create(outmean, outvar));
+}
+
+/************************************************
  * Compute statistics for log-transformed counts.
- ********************************/
+ ***********************************************/
 
 struct lognorm {
     lognorm(Rcpp::NumericVector sizefactors, double pseudo) : sf(sizefactors), ps(pseudo) {}
@@ -119,9 +169,22 @@ Rcpp::List compute_blocked_stats_lognorm(Rcpp::List bygroup, SEXP inmat, Rcpp::N
     }
 }
 
-/********************************
+// [[Rcpp::export(rng=false)]]
+Rcpp::List compute_residual_stats_lognorm(Rcpp::RObject qr, Rcpp::RObject qraux, SEXP inmat, 
+    Rcpp::NumericVector sf, double pseudo) 
+{
+    int rtype=beachmat::find_sexp_type(inmat);
+    lognorm LN(sf, pseudo);
+    if (rtype==INTSXP) {
+        return compute_residual_stats<beachmat::integer_matrix>(qr, qraux, inmat, LN);
+    } else {
+        return compute_residual_stats<beachmat::numeric_matrix>(qr, qraux, inmat, LN);
+    }
+}
+
+/*******************************************
  * Compute statistics for normalized counts.
- ********************************/
+ *******************************************/
 
 struct norm {
     norm(Rcpp::NumericVector sizefactors) : sf(sizefactors) {}
@@ -149,9 +212,9 @@ Rcpp::List compute_blocked_stats_norm(Rcpp::List bygroup, SEXP inmat, Rcpp::Nume
     }
 }
 
-/********************************
- * Compute statistics for expression values without further transformation.
- ********************************/
+/***********************************************
+ * Compute statistics for expression as provided.
+ ***********************************************/
 
 struct none {
     none() {}
@@ -166,5 +229,16 @@ Rcpp::List compute_blocked_stats_none(Rcpp::List bygroup, SEXP inmat) {
         return compute_blocked_stats<beachmat::integer_matrix>(bygroup, inmat, N);
     } else {
         return compute_blocked_stats<beachmat::numeric_matrix>(bygroup, inmat, N);
+    }
+}
+
+// [[Rcpp::export(rng=false)]]
+Rcpp::List compute_residual_stats_none(Rcpp::RObject qr, Rcpp::RObject qraux, SEXP inmat) {
+    int rtype=beachmat::find_sexp_type(inmat);
+    none N;
+    if (rtype==INTSXP) {
+        return compute_residual_stats<beachmat::integer_matrix>(qr, qraux, inmat, N);
+    } else {
+        return compute_residual_stats<beachmat::numeric_matrix>(qr, qraux, inmat, N);
     }
 }
