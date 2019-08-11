a50b603ee6e5aef6564deab578e7ab066f5c2786
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-08-10 19:46:01 -0700

    Convert scaledColRanks to use Roxygen.

--- R/scaledColRanks.R
+++ R/scaledColRanks.R
@@ -1,3 +1,50 @@
+#' Compute scaled column ranks
+#'
+#' Compute scaled column ranks from each cell's expression profile for distance calculations based on rank correlations.
+#' 
+#' @param x A numeric matrix-like object containing cells in columns and features in the rows.
+#' @param subset.row A logical, integer or character scalar indicating the rows of \code{x} to use, see \code{?"\link{scran-gene-selection}"}.
+#' @param min.mean A numeric scalar specifying the filter to be applied on the average normalized count for each feature prior to computing ranks.
+#' Disabled by setting to \code{NULL}.
+#' @param transposed A logical scalar specifying whether the output should be transposed.
+#' @param as.sparse A logical scalar indicating whether the output should be sparse.
+#' @param withDimnames A logical scalar specifying whether the output should contain the dimnames of \code{x}.
+#' 
+#' @return
+#' A matrix of the same dimensions as \code{x}, where each column contains the centred and scaled ranks of the expression values for each cell.
+#' If \code{transposed=TRUE}, this matrix is transposed so that rows correspond to cells.
+#' If \code{as.sparse}, the columns are not centered to preserve sparsity.
+#' 
+#' @details
+#' Euclidean distances computed based on the output rank matrix are equivalent to distances computed from Spearman's rank correlation.
+#' This can be used in clustering, nearest-neighbour searches, etc. as a robust alternative to Euclidean distances computed directly from \code{x}. 
+#' 
+#' If \code{as.sparse=TRUE}, the most common average rank is set to zero in the output.
+#' This can be useful for highly sparse input data where zeroes have the same rank and are themselves returned as zeroes.
+#' Obviously, this means that the ranks are not centred, so this will have to be done manually prior to any downstream distance calculations.
+#' 
+#' @author
+#' Aaron Lun
+#' 
+#' @seealso
+#' \code{\link{quickCluster}}, where this function is used.
+#' 
+#' @examples
+#' set.seed(100)
+#' popsize <- 200
+#' ngenes <- 100
+#' all.facs <- 2^rnorm(popsize, sd=0.5)
+#' counts <- matrix(rnbinom(ngenes*popsize, mu=all.facs, size=1), ncol=popsize, byrow=TRUE)
+#' 
+#' rout <- scaledColRanks(counts, transposed=TRUE)
+#' 
+#' # For use in clustering:
+#' d <- dist(rout)
+#' table(cutree(hclust(d), 4))
+#' 
+#' g <- buildSNNGraph(rout, transposed=TRUE)
+#' table(igraph::cluster_walktrap(g)$membership)
+#' 
 #' @export
 #' @importFrom scater calcAverage
 scaledColRanks <- function(x, subset.row=NULL, min.mean=NULL, transposed=FALSE, as.sparse=FALSE, withDimnames=TRUE)
@@ -22,5 +69,5 @@ scaledColRanks <- function(x, subset.row=NULL, min.mean=NULL, transposed=FALSE, 
         }
         dimnames(rkout) <- dn
     }
-    return(rkout)
+    rkout
 }
