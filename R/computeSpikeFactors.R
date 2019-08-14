de6f78ee40054c5572b84c651992e5ab43af989f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-08-13 23:36:16 -0700

    Switch computeSpikeFactors to use altExps, store its output in the main sizeFactors() field. Migrated docs to Roxygen, updated tests.

--- R/computeSpikeFactors.R
+++ R/computeSpikeFactors.R
@@ -1,19 +1,62 @@
+#' Normalization with spike-in counts
+#'
+#' Compute size factors based on the coverage of spike-in transcripts.
+#' 
+#' @param x A \linkS4class{SingleCellExperiment} object containing spike-in transcripts in an \code{\link{altExps}} entry.
+#'
+#' Support for spike-in transcripts in the rows of \code{x} itself is deprecated.
+#' @param spikes String or integer scalar specifying the alternative experiment containing the spike-in transcripts.
+#' @param type Deprecated, a character vector specifying which spike-in sets to use.
+#' @param assay.type A string indicating which assay contains the counts.
+#' @param sf.out Deprecated, a logical scalar indicating whether only size factors should be returned.
+#' @param general.use A logical scalar indicating whether the size factors should be stored for general use by all genes.
+#' 
+#' @details
+#' The spike-in size factor for each cell is computed from the sum of all spike-in counts in each cell.
+#' This aims to scale the counts to equalize spike-in coverage between cells,
+#' thus removing differences in coverage due to technical effects like capture or amplification efficiency.
+#' 
+#' Spike-in normalization can be helpful for preserving changes in total RNA content between cells, if this is of interest.
+#' Such changes would otherwise be lost when normalizing with methods that assume a non-DE majority.
+#' Indeed, spike-in normalization is the only available approach if a majority of genes are DE between two cell types or states.
+#'
+#' Size factors are computed by applying \code{\link{librarySizeFactors}} to the spike-in count matrix.
+#' This ensures that the mean of all size factors is unity for standardization purposes,
+#' if one were to compare expression values normalized with sets of size factors (e.g., in \code{\link{modelGeneVarWithSpikes}}).
+#'
+#' @return
+#' A modified \code{x} is returned, containing spike-in-derived size factors for all cells in \code{\link{sizeFactors}}.
+#' 
+#' @author
+#' Aaron Lun
+#' 
+#' @examples
+#' library(scater)
+#' sce <- mockSCE()
+#' sce <- computeSpikeFactors(sce, "Spikes")
+#' summary(sizeFactors(sce))
+#' 
+#' @seealso
+#' \code{\link{altExps}}, for the concept of alternative experiments.
+#'
+#' \code{\link{librarySizeFactors}}, for how size factors are derived from library sizes.
+#' 
+#' @references
+#' Lun ATL, McCarthy DJ and Marioni JC (2016). 
+#' A step-by-step workflow for low-level analysis of single-cell RNA-seq data with Bioconductor.
+#' \emph{F1000Res.} 5:2122
+#' 
 #' @export
-setGeneric("computeSpikeFactors", function(x, ...) { standardGeneric("computeSpikeFactors") })
-
-#' @importFrom SingleCellExperiment isSpike spikeNames
+#' @importFrom SingleCellExperiment isSpike spikeNames altExp
 #' @importFrom BiocGenerics sizeFactors "sizeFactors<-"
 #' @importFrom DelayedArray DelayedArray
 #' @importFrom DelayedMatrixStats colSums2
-#' @export
-setMethod("computeSpikeFactors", "SingleCellExperiment", 
-          function(x, type=NULL, assay.type="counts", sf.out=FALSE, general.use=TRUE) 
-# Uses the mean-centred total of spike-in transcripts as the size factor.
-#
-# written by Aaron Lun
-# created 17 February 2016
-{
-    .Deprecated(msg="'computeSpikeFactors' is not necessary if spike-ins are stored as 'altExps'")
+computeSpikeFactors <- function(x, spikes, type=NULL, assay.type="counts", sf.out=FALSE, general.use=TRUE) {
+    if (!missing(spikes)) {
+        sizeFactors(x) <- librarySizeFactors(altExp(x, spikes), exprs_values=assay.type)
+        return(x)
+    }
+
     if (is.null(type)) { 
         is.spike <- isSpike(x)
     } else {
@@ -42,7 +85,10 @@ setMethod("computeSpikeFactors", "SingleCellExperiment",
     # Saving size factors for general use, or for specific use by one (or all) of the spike-in sets.
     if (general.use) {
         sizeFactors(x) <- sf
-    } 
+        .Deprecated(msg="'general.use=TRUE' is deprecated.\nUse 'spikes=' instead, for spike-ins stored as 'altExps'.")
+    } else {
+        .Deprecated(msg="'computeSpikeFactors' is not necessary if spike-ins are stored as 'altExps'")
+    }
     if (is.null(type)) {
         type <- spikeNames(x)
     }
@@ -50,5 +96,4 @@ setMethod("computeSpikeFactors", "SingleCellExperiment",
         sizeFactors(x, type=f) <- sf
     }        
     x
-})
-
+}
--- tests/testthat/test-spikenorm.R
+++ tests/testthat/test-spikenorm.R
@@ -11,55 +11,18 @@ dummy[is.spike,] <- matrix(rnbinom(sum(is.spike)*ncells, mu=20, size=5), ncol=nc
 rownames(dummy) <- paste0("X", seq_len(ngenes))
 
 X <- SingleCellExperiment(list(counts=dummy))
-isSpike(X, "MySpike") <- is.spike
+X <- splitSCEByAlt(X, ifelse(is.spike, "ERCC", "gene"))
 
 test_that("computeSpikeFactors calculates spike-based size factors correctly", {
-    out <- computeSpikeFactors(X)
+    out <- computeSpikeFactors(X, "ERCC")
     ref <- colSums(dummy[is.spike,])
     expect_equal(unname(sizeFactors(out)), ref/mean(ref))
-    expect_equal(sizeFactors(out), sizeFactors(out, type="MySpike"))
-})
-
-test_that("computeSpikeFactors works with multiple spike-in sets", {
-    subset <- split(which(is.spike), rep(1:2, length.out=sum(is.spike)))
-    isSpike(X, "MySpike") <- subset[[1]]
-    isSpike(X, "SecondSpike") <- subset[[2]]
-    out <- computeSpikeFactors(X)
-
-    out.sub <- computeSpikeFactors(X, type="MySpike") # Sanity check, to make sure that it's calculating it differently for each spike-in.
-    subref <- colSums(dummy[subset[[1]],])
-    expect_equal(unname(sizeFactors(out.sub)), subref/mean(subref))
-    expect_equal(sizeFactors(out.sub), sizeFactors(out.sub, type="MySpike"))
-    expect_identical(sizeFactors(out.sub, type="SecondSpike"), NULL)
-    
-    out2 <- computeSpikeFactors(X)
-    expect_equal(sizeFactors(out), sizeFactors(out2))
-    expect_equal(sizeFactors(out), sizeFactors(out2, type="MySpike"))
-    expect_equal(sizeFactors(out), sizeFactors(out2, type="SecondSpike"))
-    
-    out2 <- computeSpikeFactors(X, type=c("MySpike", "SecondSpike"))
-    expect_equal(sizeFactors(out), sizeFactors(out2))
-    expect_equal(sizeFactors(out), sizeFactors(out2, type="MySpike"))
-    expect_equal(sizeFactors(out), sizeFactors(out2, type="SecondSpike"))
-})
-
-test_that("computeSpikeFactors responds correctly to general.use", {
-    sizeFactors(X) <- 1
-    out <- computeSpikeFactors(X, general.use=FALSE)
-    expect_equal(unname(sizeFactors(out)), rep(1, ncells))
-    ref <- colSums(dummy[is.spike,])
-    expect_equal(unname(sizeFactors(out, type="MySpike")), ref/mean(ref))
 })
 
 test_that("computeSpikeFactors fails correctly on silly inputs", {
-    expect_error(out <- computeSpikeFactors(X[0,]), "no spike-in transcripts present in 'x'")
-
-    alt.X <- X
-    counts(alt.X)[] <- 0L
-    expect_warning(out <- computeSpikeFactors(alt.X), "zero spike-in counts during spike-in normalization")
-    expect_identical(unname(sizeFactors(out)), rep(NaN, ncol(out)))
+    expect_error(computeSpikeFactors(X, "ERR"))
 
     # Checking that it correctly returns nothing.
-    out <- computeSpikeFactors(X[,0])
+    out <- computeSpikeFactors(X[,0], "ERCC")
     expect_identical(unname(sizeFactors(out)), numeric(0))
 })
