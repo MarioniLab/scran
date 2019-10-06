b20c974f66dfa11b1884a363ba8b24105a391006
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-10-06 12:32:49 -0700

    Switched findMarkers to using groups= instead of clusters. Added option to include row metadata in output. Updated tests.

--- R/findMarkers.R
+++ R/findMarkers.R
@@ -14,6 +14,10 @@
 #' @inheritParams combineMarkers
 #' @param log.p A logical scalar indicating if log-transformed p-values/FDRs should be returned.
 #' @param subset.row See \code{?"\link{scran-gene-selection}"}.
+#' @param row.data A \linkS4class{DataFrame} containing additional row metadata for each gene in \code{x},
+#' to be included in each of the output DataFrames.
+#' If \code{sorted=TRUE}, this should have the same row names as the output of \code{\link{combineMarkers}}
+#' (usually \code{rownames(x)}, see the \code{gene.names} argument in functions like \code{\link{pairwiseTTests}}.
 #' @param ... For the generic, further arguments to pass to specific methods.
 #'
 #' For the ANY method:
@@ -31,7 +35,7 @@
 #' @param get.spikes See \code{?"\link{scran-gene-selection}"}.
 #' 
 #' @details
-#' This function provides a convenience wrapper for marker gene identification,
+#' This function provides a convenience wrapper for marker gene identification between groups of cells,
 #' based on running \code{\link{pairwiseTTests}} or related functions and passing the result to \code{\link{combineMarkers}}.
 #' All of the arguments above are supplied directly to one of these two functions -
 #' refer to the relevant function's documentation for more details.
@@ -46,14 +50,18 @@
 #' regardless of the value of \code{log.p}.
 #' Log-transformed p-values and FDRs are reported using the natural base.
 #'
-#' The choice of \code{pval.type} determines whether the highly ranked genes are those that are DE between the current cluster
-#' and any other cluster (\code{"any"}); all other clusters (\code{"all"}); or some other clusters (\code{"some"}).
+#' The choice of \code{pval.type} determines whether the highly ranked genes are those that are DE between the current group and:
+#' \itemize{
+#' \item any other group (\code{"any"})
+#' \item all other groups (\code{"all"})
+#' \item some other groups (\code{"some"})
+#' }
 #' See \code{?\link{combineMarkers}} for more details.
 #'
 #' @return 
-#' A named list of \linkS4class{DataFrame}s, each of which contains a sorted marker gene list for the corresponding cluster.
-#' In each DataFrame, the top genes are chosen to enable separation of that cluster from all other clusters.
-#' Log-fold changes are reported as differences in average \code{x} between clusters
+#' A named list of \linkS4class{DataFrame}s, each of which contains a sorted marker gene list for the corresponding group.
+#' In each DataFrame, the top genes are chosen to enable separation of that group from all other groups.
+#' Log-fold changes are reported as differences in average \code{x} between groups
 #' (usually in base 2, depending on the transformation applied to \code{x}).
 #' 
 #' See \code{?\link{combineMarkers}} for more details on the output format.
@@ -77,26 +85,28 @@
 #' # Any clustering method is okay, only using k-means for convenience.
 #' kout <- kmeans(t(logcounts(sce)), centers=4) 
 #'
-#' out <- findMarkers(sce, clusters=kout$cluster)
+#' out <- findMarkers(sce, groups=kout$cluster)
 #' names(out)
 #' out[[1]]
 #'
 #' # More customization of the tests:
-#' out <- findMarkers(sce, clusters=kout$cluster, test.type="wilcox")
+#' out <- findMarkers(sce, groups=kout$cluster, test.type="wilcox")
 #' out[[1]]
 #'
-#' out <- findMarkers(sce, clusters=kout$cluster, lfc=1, direction="up")
+#' out <- findMarkers(sce, groups=kout$cluster, lfc=1, direction="up")
 #' out[[1]]
 #'
-#' out <- findMarkers(sce, clusters=kout$cluster, pval.type="all")
+#' out <- findMarkers(sce, groups=kout$cluster, pval.type="all")
 #' out[[1]]
 #'
 #' @name findMarkers
 NULL
 
 #' @importFrom BiocParallel SerialParam
-.findMarkers <- function(x, clusters, test.type=c("t", "wilcox", "binom"),
-    ..., pval.type=c("any", "some", "all"), log.p=FALSE, full.stats=FALSE, sorted=TRUE) 
+#' @importFrom BiocGenerics cbind
+.findMarkers <- function(x, groups, test.type=c("t", "wilcox", "binom"),
+    ..., pval.type=c("any", "some", "all"), log.p=FALSE, full.stats=FALSE, 
+    sorted=TRUE, row.data=NULL) 
 {
     test.type <- match.arg(test.type)
     if (test.type=="t") {
@@ -110,9 +120,25 @@ NULL
         effect.field <- "logFC"
     }
 
-    fit <- FUN(x, clusters, ..., log.p=TRUE)
-    combineMarkers(fit$statistics, fit$pairs, pval.type=pval.type, log.p.in=TRUE, log.p.out=log.p, 
+    fit <- FUN(x, groups, ..., log.p=TRUE)
+    output <- combineMarkers(fit$statistics, fit$pairs, pval.type=pval.type, log.p.in=TRUE, log.p.out=log.p, 
         full.stats=full.stats, pval.field="log.p.value", effect.field=effect.field, sorted=sorted)
+
+    if (!is.null(row.data)) {
+        for (i in seq_along(output)) {
+            if (sorted) {
+                if (!identical(sort(rownames(output[[i]])), sort(rownames(row.data)))) {
+                    stop("non-identical row names for 'row.data' and result tables")
+                }
+                cur.row.data <- row.data[rownames(output[[i]]),,drop=FALSE]
+            } else {
+                cur.row.data <- row.data
+                rownames(cur.row.data) <- rownames(output[[i]])
+            }
+            output[[i]] <- cbind(cur.row.data, output[[i]])
+        }
+    }
+    output
 }
 
 #' @export
--- tests/testthat/test-markers.R
+++ tests/testthat/test-markers.R
@@ -7,44 +7,59 @@ ngenes <- 250
 means <- 2^runif(ngenes, -1, 5)
 dummy <- matrix(rnbinom(ngenes*ncells, mu=means, size=5), ncol=ncells, nrow=ngenes)
 
+library(scater)
 rownames(dummy) <- paste0("X", seq_len(ngenes))
 X <- SingleCellExperiment(list(counts=dummy))
 sizeFactors(X) <- colSums(dummy)
-X <- normalize(X)
+X <- logNormCounts(X)
 
 test_that("findMarkers works correctly with subsetting and spikes", {   
     # Works with an SCE object.
     clust <- kmeans(t(exprs(X)), centers=3)
-    out <- findMarkers(X, clusters=clust$cluster)
-    out2 <- findMarkers(exprs(X), clusters=clust$cluster)
+    out <- findMarkers(X, groups=clust$cluster)
+    out2 <- findMarkers(exprs(X), groups=clust$cluster)
     expect_identical(out, out2)
 
     # Works with subsetting.
-    out <- findMarkers(X, clusters=clust$cluster, subset.row=100:1)
-    out2 <- findMarkers(X[100:1,], clusters=clust$cluster)
+    out <- findMarkers(X, groups=clust$cluster, subset.row=100:1)
+    out2 <- findMarkers(X[100:1,], groups=clust$cluster)
     expect_identical(out, out2)
 
-    out <- findMarkers(X, clusters=clust$cluster, subset.row=1:10, full.stats=TRUE) 
-    out2 <- findMarkers(X[1:10,], clusters=clust$cluster, full.stats=TRUE)
+    out <- findMarkers(X, groups=clust$cluster, subset.row=1:10, full.stats=TRUE) 
+    out2 <- findMarkers(X[1:10,], groups=clust$cluster, full.stats=TRUE)
     expect_identical(out, out2)
     expect_identical(rownames(out[[1]]$stats.2), rownames(out[[1]])) # names propagate to internal DFs.
 
-    out <- findMarkers(X, clusters=clust$cluster, subset.row=2:11, full.stats=TRUE, gene.names=NULL) # switches to using 'subset.row' as the row names. 
-    out2 <- findMarkers(unname(X)[2:11,], clusters=clust$cluster, full.stats=TRUE, gene.names=2:11)
+    # Switches to using 'subset.row' as the row names. 
+    out <- findMarkers(X, groups=clust$cluster, subset.row=2:11, full.stats=TRUE, gene.names=NULL) 
+    out2 <- findMarkers(unname(X)[2:11,], groups=clust$cluster, full.stats=TRUE, gene.names=2:11)
     expect_identical(out, out2)
     expect_identical(rownames(out[[1]]$stats.2), rownames(out[[1]])) 
     expect_identical(sort(as.integer(rownames(out[[1]]))), 2:11)
 
-    # Works with spikes.
-    isSpike(X, "ERCC") <- 1:100
-    out <- findMarkers(X, clusters=clust$cluster)
-    out2 <- findMarkers(exprs(X)[-(1:100),], clusters=clust$cluster)
-    expect_identical(out, out2)
-
     # Repeating with a design matrix, to check that subsetting works in both branches for coefficient calculation.
     block <- factor(sample(2, ncol(X), replace=TRUE))
     design <- model.matrix(~block)[,-1,drop=FALSE]
-    out.des <- findMarkers(exprs(X), clusters=clust$cluster, design=design, subset.row=100:1)
-    out.des2 <- findMarkers(exprs(X)[100:1,,drop=FALSE], clusters=clust$cluster, design=design)
+    out.des <- findMarkers(exprs(X), groups=clust$cluster, design=design, subset.row=100:1)
+    out.des2 <- findMarkers(exprs(X)[100:1,,drop=FALSE], groups=clust$cluster, design=design)
     expect_identical(out.des, out.des2)
+})
+
+test_that("findMarkers works correctly with row metadata", {
+    clust <- kmeans(t(exprs(X)), centers=3)
+    meta <- DataFrame(Y=runif(nrow(X)), row.names=rownames(dummy))
+    out <- findMarkers(dummy, groups=clust$cluster, row.data=meta)
+
+    for (i in seq_along(out)) {
+        x <- out[[i]]
+        expect_identical(x$Y, meta[rownames(x),"Y"])
+    }
+
+    # Handles it without names.
+    out <- findMarkers(dummy, groups=clust$cluster, sorted=FALSE, gene.names=NULL, row.data=meta)
+    for (i in seq_along(out)) {
+        x <- out[[i]]
+        expect_identical(rownames(x), as.character(seq_len(nrow(dummy))))
+        expect_identical(x$Y, meta$Y)
+    } 
 })
