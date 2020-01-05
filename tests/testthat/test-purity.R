cd8000ea7c73ad42a51f172d0b007ebfd0bb703a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-04 21:56:37 -0800

    Updated purity tests.

--- tests/testthat/test-purity.R
+++ tests/testthat/test-purity.R
@@ -7,36 +7,48 @@ test_that('clusterPurity yields correct output for pure clusters', {
     y <- matrix(clusters, nrow=50, ncol=length(clusters), byrow=TRUE)
     y <- jitter(y)
 
-    out <- clusterPurity(y, clusters, pseudo.count=0)
+    out <- clusterPurity(y, clusters)
     expect_true(all(out==1))
     expect_identical(length(out), ncol(y))
 })
 
 set.seed(70001)
 test_that('clusterPurity yields correct output for compromised clusters', {
-    y <- matrix(seq_len(100), nrow=50, ncol=100, byrow=TRUE)
+    y <- matrix(rep(0:1, each=100), nrow=50, ncol=200, byrow=TRUE)
     y <- jitter(y)
-    clusters <- rep(1:2, c(1, 99))
-    out <- clusterPurity(y, clusters, pseudo.count=0)
+
+    clusters <- rep(1:2, each=100)
+    clusters[1] <- 2
+    out <- clusterPurity(y, clusters)
 
     expect_true(out[1] <= 0.05)
     expect_true(all(out[-1] > 0.9))
-    expect_false(is.unsorted(out))
 })
 
 set.seed(700011)
-test_that('clusterPurity responds to the pseudo-count', {
-    y <- matrix(rnorm(10000), nrow=50)
-    clusters <- sample(1:5, ncol(y), replace=TRUE)
+test_that("clusterPurity handles correctly with the weighting", {
+    # Creating a bulk of points.
+    y0 <- matrix(rnorm(10000), nrow=50)
+    y1 <- matrix(1000, nrow=50, ncol=10)
+    y2 <- matrix(1000, nrow=50, ncol=10)
 
-    out1 <- clusterPurity(y, clusters, pseudo.count=0)
-    out2 <- clusterPurity(y, clusters, pseudo.count=1)
+    y <- cbind(y1, y2, y0)
+    clusters <- rep(1:3, c(ncol(y1), ncol(y2), ncol(y0)))
+    out1 <- clusterPurity(y, clusters)
+    expect_true(all(abs(out1[1:20]-0.5) < 1e-8))
+    expect_true(all(out1[-(1:20)]==1))
 
-    # Greater shrinkage towards 0.5.
-    diff1 <- out1 - 0.5
-    diff2 <- out2 - 0.5
-    expect_true(all(sign(diff1)==sign(diff2)))
-    expect_true(all(sign(diff1) * diff1 > sign(diff2) * diff2))
+    # Unaffected by changes in the number of cells. Technically this should be
+    # identical(), but the order of cells returned by findNeighbors() changes,
+    # and this results in numeric precision changes due to order of addition of
+    # double-precision values, resulting in very slightly different output.
+    sub <- c(1:5, ncol(y1) + seq_len(ncol(y2) + ncol(y0)))
+    out2 <- clusterPurity(y[,sub], clusters[sub])
+    expect_equal(out1[sub], out2)
+
+    sub <- c(1, ncol(y1) + seq_len(ncol(y2) + ncol(y0)))
+    out2 <- clusterPurity(y[,sub], clusters[sub])
+    expect_equal(out1[sub], out2)
 })
 
 set.seed(70002)
@@ -47,22 +59,22 @@ test_that('clusterPurity handles SCEs correctly', {
     g <- buildSNNGraph(sce)
     clusters <- igraph::cluster_walktrap(g)$membership
 
-    expect_identical(
+    expect_equal(
         clusterPurity(logcounts(sce), clusters),
         clusterPurity(sce, clusters)
     )
 
     # Handles subsetting correctly.
-    expect_identical(
+    expect_equal(
         clusterPurity(sce, clusters, subset.row=1:10),
         clusterPurity(sce[1:10,], clusters)
     )
 
     # Also deals with reducedDims.
     sce <- runPCA(sce)
-    expect_identical(
-        out <- clusterPurity(sce, clusters, dimred="PCA"),
+    expect_equal(
+        out <- clusterPurity(sce, clusters, use.dimred="PCA"),
         clusterPurity(reducedDim(sce), clusters, transposed=TRUE)
     )
-    expect_identical(length(out), ncol(sce))
+    expect_equal(length(out), ncol(sce))
 })
