4b29f1633be997271bfb9c9c68a7933475806990
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-04 21:35:41 -0800

    Replaced pseudo-count with cluster size weighting for clusterPurity.

--- R/clusterPurity.R
+++ R/clusterPurity.R
@@ -11,7 +11,6 @@
 #' @param k Integer scalar specifying the number of nearest neighbors to use to determine the radius of the hyperspheres.
 #' @param transposed Logical scalar specifying whether \code{x} contains cells in the rows.
 #' @param subset.row See \code{?"\link{scran-gene-selection}"}.
-#' @param pseudo.count Numeric scalar specifying the pseudo-count to use for purity calculations.
 #' @param BNPARAM A \linkS4class{BiocNeighborParam} object specifying the nearest neighbor algorithm.
 #' This should be an algorithm supported by \code{\link{findNeighbors}}.
 #' @param BPPARAM A \linkS4class{BiocParallelParam} object indicating whether and how parallelization should be performed across genes.
@@ -22,12 +21,12 @@
 #' @details
 #' The purity of a cluster is quantified by creating a hypersphere around each cell in the cluster
 #' and computing the proportion of cells in that hypersphere from the same cluster.
-#' If all cells in a cluster have proportions close to 1, this indicate that the cluster is highly pure,
-#' i.e., there are no cells from other clusters in its region of the coordinate space.
+#' If all cells in a cluster have proportions close to 1, this indicates that the cluster is highly pure,
+#' i.e., there are few cells from other clusters in its region of the coordinate space.
+#' The distribution of purities for each cluster can be used as a measure of separation from other clusters.
 #'
-#' A more common scenario is that a fraction of cells will have low purity values;
-#' this corresponds to cells lying at the boundaries of two adjacent clusters,
-#' whereas cells close to the cluster center will have relatively high purity.
+#' In most cases, the majority of cells of a cluster will have high purities, corresponding to cells close to the cluster center;
+#' and a fraction will have low values, corresponding to cells lying at the boundaries of two adjacent clusters,
 #' A high degree of over-clustering will manifest as a majority of cells with purities close to zero.
 #' 
 #' The choice of \code{k} is used only to determine an appropriate value for the hypersphere radius.
@@ -35,9 +34,10 @@
 #' in contrast to computing purity based on the proportion of k-nearest neighbors in the same cluster.
 #' For example, the latter will fail most obviously when the size of the cluster is less than \code{k}.
 #'
-#' Technically, purity values are computed by adding \code{pseudo.count} to the number of cells in the same cluster
-#' and dividing by the total number of cells in the hypersphere plus \code{2*pseudo.count}.
-#' This shrinks all values towards 0.5 and will yield a more conservative estimate of purity when few cells are available.
+#' Technically, purity values are computed after weighting each cell by the reciprocal of the number of cells in the same cluster.
+#' Otherwise, clusters with more cells will have higher purities as any contamination is offset by the bulk of cells.
+#' By comparison, an adjacent cluster with few cells will have lower purities.
+#' Without weighting, this effect would compromise comparisons between clusters.
 #'
 #' @author Aaron Lun
 #' @examples
@@ -53,7 +53,7 @@
 #' # Mocking up a stronger example:
 #' ngenes <- 1000
 #' centers <- matrix(rnorm(ngenes*3), ncol=3)
-#' clusters <- sample(1:3, 500, replace=TRUE)
+#' clusters <- sample(1:3, ncol(sce), replace=TRUE)
 #'
 #' y <- centers[,clusters]
 #' y <- y + rnorm(length(y))
@@ -69,8 +69,11 @@ NULL
 #' @importFrom scater .bpNotSharedOrUp
 #' @importFrom Matrix t
 #' @importFrom stats median
+#' @importFrom S4Vectors List
+#' @importClassesFrom IRanges IntegerList LogicalList
+#' @importMethodsFrom BiocGenerics relist
 .cluster_purity <- function(x, clusters, k=50, transposed=FALSE, subset.row=NULL,
-    pseudo.count=1, BNPARAM=KmknnParam(), BPPARAM=SerialParam()) 
+    BNPARAM=KmknnParam(), BPPARAM=SerialParam()) 
 {
     if (!transposed) {
         if (!is.null(subset.row)) {
@@ -90,13 +93,18 @@ NULL
     dist <- median(findKNN(BNINDEX=idx, k=k, BNPARAM=BNPARAM, BPPARAM=BPPARAM, last=1, get.index=FALSE)$distance)
     nout <- findNeighbors(BNINDEX=idx, threshold=dist, BNPARAM=BNPARAM, get.distance=FALSE)$index
 
-    # Counting the number of entities in the same cluster as me.
-    # This could be made much more efficient.
-    sameness <- clusters[unlist(nout)]==rep(clusters, lengths(nout))
-    samelist <- relist(sameness, nout)
-    nsame <- vapply(samelist, sum, 0L)
+    nout <- List(nout)
+    clust.ids <- relist(clusters[unlist(nout)], nout)
 
-    (nsame + pseudo.count)/(lengths(samelist) + 2*pseudo.count)
+    w <- 1/table(clusters)
+    weights <- relist(as.numeric(w[unlist(clust.ids)]), clust.ids)
+    total <- sum(weights)
+
+    self.ids <- relist(rep(clusters, lengths(nout)), nout)
+    is.same <- clust.ids==self.ids
+    same <- sum(weights[is.same])
+
+    same/total
 }
 
 #' @export
