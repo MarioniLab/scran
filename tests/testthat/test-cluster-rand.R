fa7a71c6d41ff8350db93d4675fbeeee25e67711
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-10 11:55:59 -0700

    Added tests for clusterRand.

---
+++ tests/testthat/test-cluster-rand.R
@@ -0,0 +1,66 @@
+# Tests the clusterRand() function.
+# library(testthat); library(scran); source('setup.R'); source('test-cluster-rand.R')
+
+library(scater)
+sce <- mockSCE(ncells=200)
+sce <- logNormCounts(sce)
+
+set.seed(8000)
+clust1 <- kmeans(t(logcounts(sce)),3)$cluster
+clust2 <- kmeans(t(logcounts(sce)),5)$cluster
+
+test_that("clusterRand gives output of the right shape", {
+    ratio <- clusterRand(clust1, clust2)
+
+    expect_identical(dim(ratio), c(3L, 3L))
+    expect_identical(rownames(ratio), as.character(1:3))
+    expect_identical(colnames(ratio), as.character(1:3))
+
+    expect_true(all(is.na(ratio[lower.tri(ratio)])))
+    expect_true(all(!is.na(diag(ratio))))
+    expect_true(all(!is.na(ratio[upper.tri(ratio)])))
+})
+
+test_that("clusterRand mimics the original rand index", {
+    full <- clusterRand(clust1, clust2, as.ratio=FALSE)
+
+    # Applying a reference calculation.
+    status1 <- outer(clust1, clust1, "==")
+    status2 <- outer(clust2, clust2, "==")
+
+    keep <- upper.tri(status1)
+    status1 <- status1[keep]
+    status2 <- status2[keep]
+
+    a <- sum(status1 & status2)
+    b <- sum(!status2 & !status1)
+
+    expect_equal(a, sum(diag(full$correct)))
+    expect_equal(b, sum(full$correct[upper.tri(full$correct)]))
+
+    rand <- sum(full$correct, na.rm=TRUE)/sum(full$total, na.rm=TRUE)
+    expect_identical(rand, (a+b)/choose(length(clust1), 2))
+})
+
+test_that("clusterRand handles silly inputs", {
+    ref <- clusterRand(clust1, clust2)
+
+    # Respects empty levels in 'ref'.
+    clustX <- factor(clust1, levels=1:5)
+    out <- clusterRand(clustX, clust2)
+
+    expect_identical(dim(out), c(5L, 5L))
+    expect_identical(rownames(out), as.character(1:5))
+    expect_identical(colnames(out), as.character(1:5))
+    expect_identical(out[1:3,1:3], ref)
+
+    # Ignores empty levels in 'alt'.
+    clustY <- factor(clust2, levels=1:5)
+    out <- clusterRand(clust1, clustY)
+    expect_identical(ref, out)
+
+    # Behaves sensibly with zero-length inputs.
+    out <- clusterRand(clust1[0], clust2[0])
+    expect_identical(dim(out), c(0L, 0L))
+})
+
